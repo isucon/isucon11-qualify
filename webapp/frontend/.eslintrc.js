a74083fffd7daadca85afd2f99ec46ea3cd05893
ryoha000 <the7uya@gmail.com>
2021-06-11 03:46:55 +0900

    feat(frontend): eslint + Prettierの導入

---
+++ webapp/frontend/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    commonjs: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  settings: {
+    react: { version: 'detect' }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  plugins: ['react-hooks', 'react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+}
