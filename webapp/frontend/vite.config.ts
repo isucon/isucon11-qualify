fc8bcec20ea21f379fc0aa90f601faef99be9a7e
ryoha000 <the7uya@gmail.com>
2021-06-11 03:47:56 +0900

    feat(frontend): format

--- webapp/frontend/vite.config.ts
+++ webapp/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import WindiCSS from "vite-plugin-windicss"
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
