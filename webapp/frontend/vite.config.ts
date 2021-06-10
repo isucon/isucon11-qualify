7e5c36bfaf560e88ef35f2ca7479be07fd1eee2e
ryoha000 <the7uya@gmail.com>
2021-06-11 01:08:52 +0900

    feat(frontend): windicssの追加

--- webapp/frontend/vite.config.ts
+++ webapp/frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import WindiCSS from "vite-plugin-windicss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), WindiCSS()]
 })
