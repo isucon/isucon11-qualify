7d6cf6557b601dcb4d29e9919a2f40ca36f7f9a2
Nagarei <reiji.nagata@ba2.so-net.ne.jp>
2021-06-12 09:19:40 +0900

    feat(bench): add catalog

--- extra/ISUAPI_standalone/isuapi.go
+++ extra/ISUAPI_standalone/isuapi.go
@@ -39,9 +39,18 @@ func init() {
 			Name:        "isu0",
 			LimitWeight: 150,
 			Weight:      30,
-			Size:        "W65.5×D66×H114.5~128.5(SH43~52)cm",
+			Size:        "W65.5×D66×H114.5~128.5cm",
 			Maker:       "isu maker",
 			Features:    "headrest,armrest",
+		},
+		"562dc0df-2d4f-4e38-98c0-9333f4ff3e38": {
+			ID:          "550e8400-e29b-41d4-a716-446655440000",
+			Name:        "isu1",
+			LimitWeight: 136,
+			Weight:      15,
+			Size:        "W47×D43×H91cm～97cm",
+			Maker:       "isu maker 2",
+			Features:    "",
 		},
 	}
 }
