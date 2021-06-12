a6da8090c6af3bef1b14dc297de0798b51ca3a20
Nagarei <reiji.nagata@ba2.so-net.ne.jp>
2021-06-12 13:50:50 +0900

    feat(bench): postActivate/Deactivate
    ref #40

--- extra/ISUAPI_standalone/isuapi.go
+++ extra/ISUAPI_standalone/isuapi.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,7 +14,13 @@ import (
 )
 
 var (
-	catalogs map[string]*IsuCatalog
+	privateIPBlocks []*net.IPNet
+
+	catalogs        map[string]*IsuCatalog
+	validIsu        map[string]IsuState
+	charactorList   []string
+	activatedIsu    = map[string]ActivatedIsuState{} //key=isuID+targetIP
+	activatedIsuMtx = sync.Mutex{}
 )
 
 type IsuCatalog struct {
@@ -24,12 +33,41 @@ type IsuCatalog struct {
 	Features    string `json:"features"`
 }
 
-func getEnv(key, defaultValue string) string {
+type IsuState struct {
+	CatalogID string `json:"catalog_id"`
+	Charactor string `json:"charactor"`
+}
+type ActivatedIsuState struct {
+	cancelFunc context.CancelFunc
+}
+
+type IsuConditionPosterState struct {
+	targetIP string
+	isuID    string
+}
+
+func getEnv(key string, defaultValue string) string {
 	val := os.Getenv(key)
 	if val != "" {
 		return val
 	}
 	return defaultValue
+}
+func isPrivateIP(ipstr string) bool {
+	ip := net.ParseIP(ipstr)
+	if ip == nil {
+		return false
+	}
+
+	if ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast() {
+		return true
+	}
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	return false
 }
 
 func init() {
@@ -52,6 +90,37 @@ func init() {
 			Maker:       "isu maker 2",
 			Features:    "",
 		},
+	}
+	charactorList = []string{"Normal", "clean freak"}
+	validIsu = map[string]IsuState{
+		"0694e4d7-dfce-4aec-b7ca-887ac42cfb8f": {CatalogID: "550e8400-e29b-41d4-a716-446655440000", Charactor: charactorList[0]},
+		"3a8ae675-3702-45b5-b1eb-1e56e96738ea": {CatalogID: "550e8400-e29b-41d4-a716-446655440000", Charactor: charactorList[1]},
+		"3efff0fa-75bc-4e3c-8c9d-ebfa89ecd15e": {CatalogID: "550e8400-e29b-41d4-a716-446655440000", Charactor: charactorList[0]},
+		"f67fcb64-f91c-4e7b-a48d-ddf1164194d0": {CatalogID: "550e8400-e29b-41d4-a716-446655440000", Charactor: charactorList[1]},
+		"32d1c708-e6ef-49d0-8ca9-4fd51844dcc8": {CatalogID: "550e8400-e29b-41d4-a716-446655440000", Charactor: charactorList[0]},
+		"f012233f-c50e-4349-9473-95681becff1e": {CatalogID: "562dc0df-2d4f-4e38-98c0-9333f4ff3e38", Charactor: charactorList[1]},
+		"af64735c-667a-4d95-a75e-22d0c76083e0": {CatalogID: "562dc0df-2d4f-4e38-98c0-9333f4ff3e38", Charactor: charactorList[0]},
+		"cb68f47f-25ef-46ec-965b-d72d9328160f": {CatalogID: "562dc0df-2d4f-4e38-98c0-9333f4ff3e38", Charactor: charactorList[1]},
+		"57d600ef-15b4-43bc-ab79-6399fab5c497": {CatalogID: "562dc0df-2d4f-4e38-98c0-9333f4ff3e38", Charactor: charactorList[0]},
+		"aa0844e6-812d-41d2-908a-eeb82a50b627": {CatalogID: "562dc0df-2d4f-4e38-98c0-9333f4ff3e38", Charactor: charactorList[1]},
+	}
+
+	//privateIPBlocks
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"169.254.0.0/16", // RFC3927 link-local
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, err := net.ParseCIDR(cidr)
+		if err != nil {
+			panic(fmt.Errorf("parse error on %q: %v", cidr, err))
+		}
+		privateIPBlocks = append(privateIPBlocks, block)
 	}
 }
 
@@ -90,14 +159,70 @@ func getCatalog(c echo.Context) error {
 }
 
 func postActivate(c echo.Context) error {
-	return fmt.Errorf("not implemented")
+	isuID := c.FormValue("isu_id")
+	if isuID == "" {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	if _, ok := validIsu[isuID]; !ok {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	state := IsuConditionPosterState{
+		targetIP: c.RealIP(),
+		isuID:    isuID,
+	}
+	if !isPrivateIP(state.targetIP) {
+		return c.NoContent(http.StatusForbidden)
+	}
+	key := isuID + state.targetIP
+
+	ctx, cancel := context.WithCancel(context.Background())
+	conflict := func() bool {
+		activatedIsuMtx.Lock()
+		defer activatedIsuMtx.Unlock()
+		if _, ok := activatedIsu[key]; ok {
+			return true
+		}
+		activatedIsu[key] = ActivatedIsuState{cancelFunc: cancel}
+		return false
+	}()
+	if !conflict {
+		go isuConditionPoster(ctx, state)
+	}
+
+	return c.JSON(http.StatusAccepted, validIsu[isuID])
 }
 
 func postDeactivate(c echo.Context) error {
-	return fmt.Errorf("not implemented")
+	isuID := c.FormValue("isu_id")
+	if isuID == "" {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	if _, ok := validIsu[isuID]; !ok {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	state := IsuConditionPosterState{
+		targetIP: c.RealIP(),
+		isuID:    isuID,
+	}
+	key := isuID + state.targetIP
+
+	func() {
+		activatedIsuMtx.Lock()
+		defer activatedIsuMtx.Unlock()
+		activatedIsu[key].cancelFunc()
+		delete(activatedIsu, key)
+	}()
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 func postDie(c echo.Context) error {
 	os.Exit(0)
 	return nil
+}
+
+func isuConditionPoster(ctx context.Context, state IsuConditionPosterState) {
+	// TODO
 }
