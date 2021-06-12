911f5b92abe7258340ac6cbc452772bb35c74246
ShotaKitazawa <skitazawa1121@gmail.com>
2021-06-12 15:14:19 +0900

    fix #38

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -1,24 +1,20 @@
 DROP TABLE IF EXISTS `isu`;
 CREATE TABLE `isu` (
-  `id` VARCHAR(255) PRIMARY KEY,
+  `uuid` CHAR(36) PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `image` LONGBLOB,
   `catalog_id` VARCHAR(255) NOT NULL,
   `user_id` BIGINT NOT NULL,
-  `is_deleted` TINYINT(1) DEFAULT FALSE,
+  `is_deleted` TINYINT(1) NOT NULL DEFAULT FALSE,
   `created_at` DATETIME(6) NOT NULL,
   `updated_at` DATETIME(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
--- condition は結局 JSON 何だっけ？
 DROP TABLE IF EXISTS `isu_log`;
 CREATE TABLE `isu_log` (
   `isu_id` VARCHAR(255),
-  `timestamp` DATETIME(6),
-  `condition` JSON NOT NULL,
--- `is_dirty` TINYINT(1) DEFAULT FALSE,
--- `is_overweight` TINYINT(1) DEFAULT FALSE,
--- `is_broken` TINYINT(1) DEFAULT FALSE,
+  `timestamp` DATETIME,
+  `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) NOT NULL,
   PRIMARY KEY(`isu_id`,`timestamp`)
@@ -27,7 +23,7 @@ CREATE TABLE `isu_log` (
 DROP TABLE IF EXISTS `graph`;
 CREATE TABLE `graph` (
   `isu_id` VARCHAR(255),
-  `start_at` DATETIME(6),
+  `start_at` DATETIME,
   `data` JSON NOT NULL,
   `created_at` DATETIME(6) NOT NULL,
   `updated_at` DATETIME(6) NOT NULL,
@@ -36,13 +32,11 @@ CREATE TABLE `graph` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   `created_at` DATETIME(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
--- 以下の図より、平常時とベンチ時とで何かを切り替えるのはベンチ側であり App 側は何もしなくて良さそうなので、以下のテーブルは要らないかも？
--- https://docs.google.com/presentation/d/1yzeYYi39hW66D42u7TrY7zLCz0N1RQyatBv8n49b5Xw/edit#slide=id.gdd6b21deeb_0_5
 DROP TABLE IF EXISTS `isu_association_config`;
 CREATE TABLE `isu_association_config` (
   `name` VARCHAR(255) PRIMARY KEY,
