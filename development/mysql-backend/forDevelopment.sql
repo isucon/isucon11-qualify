3966d6d0930c60a66bffc5b13379ab02d2b59e87
ShotaKitazawa <skitazawa1121@gmail.com>
2021-06-14 18:24:36 +0900

    fix schema

--- development/mysql-backend/forDevelopment.sql
+++ development/mysql-backend/forDevelopment.sql
@@ -1,3 +1,3 @@
-ALTER TABLE isu ADD CONSTRAINT `isu_user_id` FOREIGN KEY (user_id) REFERENCES user(id);
-ALTER TABLE isu_log ADD CONSTRAINT `isu_log_isu_uuid` FOREIGN KEY (isu_id) REFERENCES isu(uuid);
-ALTER TABLE graph ADD CONSTRAINT `graph_isu_uuid` FOREIGN KEY (isu_id) REFERENCES isu(uuid);
+ALTER TABLE isu ADD CONSTRAINT `isu_user_id` FOREIGN KEY (jia_user_id) REFERENCES user(jia_user_id);
+ALTER TABLE isu_log ADD CONSTRAINT `isu_log_isu_uuid` FOREIGN KEY (jia_isu_uuid) REFERENCES isu(jia_isu_uuid);
+ALTER TABLE graph ADD CONSTRAINT `graph_isu_uuid` FOREIGN KEY (jia_isu_uuid) REFERENCES isu(jia_isu_uuid);
--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -1,10 +1,13 @@
+-- TODO: 各カラムの varchar の大きさを調整する (#51)
+
 DROP TABLE IF EXISTS `isu`;
 CREATE TABLE `isu` (
-  `uuid` CHAR(36) PRIMARY KEY,
+  `jia_isu_uuid` CHAR(36) PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `image` LONGBLOB,
-  `catalog_id` VARCHAR(255) NOT NULL,
-  `user_id` BIGINT NOT NULL,
+  `character` VARCHAR(255) NOT NULL,
+  `jia_catalog_id` VARCHAR(255) NOT NULL, -- TODO: jia_catalog_id の format 策定 (#50)
+  `jia_user_id` VARCHAR(255) NOT NULL,
   `is_deleted` TINYINT(1) NOT NULL DEFAULT FALSE,
   `created_at` DATETIME(6) NOT NULL,
   `updated_at` DATETIME(6) NOT NULL
@@ -12,28 +15,27 @@ CREATE TABLE `isu` (
 
 DROP TABLE IF EXISTS `isu_log`;
 CREATE TABLE `isu_log` (
-  `isu_id` CHAR(36),
+  `jia_isu_uuid` CHAR(36),
   `timestamp` DATETIME,
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) NOT NULL,
-  PRIMARY KEY(`isu_id`,`timestamp`)
+  PRIMARY KEY(`jia_isu_uuid`,`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 DROP TABLE IF EXISTS `graph`;
 CREATE TABLE `graph` (
-  `isu_id` CHAR(36),
+  `jia_isu_uuid` CHAR(36),
   `start_at` DATETIME,
   `data` JSON NOT NULL,
   `created_at` DATETIME(6) NOT NULL,
   `updated_at` DATETIME(6) NOT NULL,
-  PRIMARY KEY(`isu_id`,`start_at`)
+  PRIMARY KEY(`jia_isu_uuid`,`start_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL UNIQUE,
+  `jia_user_id` VARCHAR(255) PRIMARY KEY,
   `created_at` DATETIME(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
