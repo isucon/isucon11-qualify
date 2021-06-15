f1b76fb86004378f7f4cd18b60b8d7ecb52f6f82
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2021-06-15 15:53:53 +0900

    fix(backend): DATETIMEのフィールドを CURRENT_TIMESTAMPで更新

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE `isu` (
   `jia_catalog_id` VARCHAR(255) NOT NULL, -- TODO: jia_catalog_id の format 策定 (#50)
   `jia_user_id` VARCHAR(255) NOT NULL,
   `is_deleted` TINYINT(1) NOT NULL DEFAULT FALSE,
-  `created_at` DATETIME(6) NOT NULL,
-  `updated_at` DATETIME(6) NOT NULL
+  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) ON UPDATE CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 DROP TABLE IF EXISTS `isu_log`;
@@ -19,7 +19,7 @@ CREATE TABLE `isu_log` (
   `timestamp` DATETIME,
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
+  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY(`jia_isu_uuid`,`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
@@ -28,15 +28,15 @@ CREATE TABLE `graph` (
   `jia_isu_uuid` CHAR(36),
   `start_at` DATETIME,
   `data` JSON NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-  `updated_at` DATETIME(6) NOT NULL,
+  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY(`jia_isu_uuid`,`start_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
-  `created_at` DATETIME(6) NOT NULL
+  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 DROP TABLE IF EXISTS `isu_association_config`;
