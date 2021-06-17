ea492b563e66fb2a1485f8f3187ed010f19e2d54
ryoha000 <the7uya@gmail.com>
2021-06-18 08:56:29 +0900

    feat(frontend) impl state

---
+++ webapp/frontend/src/context/state.tsx
@@ -0,0 +1,62 @@
+import { createContext, Dispatch, useContext, useReducer } from 'react'
+
+interface User {
+  jiaUserID: string
+}
+interface State {
+  me: User | null
+}
+type Action = { type: 'login'; user: User } | { type: 'logout' }
+
+const initialState: State = {
+  me: null
+}
+
+const stateContext = createContext<State>(initialState)
+const dispatchContext = createContext<Dispatch<Action> | null>(null)
+
+const stateReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'login': {
+      return { ...state, me: action.user }
+    }
+    case 'logout': {
+      return { ...state, me: null }
+    }
+    default: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const _exhaustiveCheck: never = action
+      throw new Error('invalid action')
+    }
+  }
+}
+
+const StateContextProvider = ({ children }: { children: JSX.Element }) => {
+  const [state, dispatch] = useReducer(stateReducer, initialState)
+
+  return (
+    <stateContext.Provider value={state}>
+      <dispatchContext.Provider value={dispatch}>
+        {children}
+      </dispatchContext.Provider>
+    </stateContext.Provider>
+  )
+}
+
+export const useStateContext = () => {
+  const state = useContext(stateContext)
+  if (!state) {
+    throw new Error('require wrapped by stateContext')
+  }
+  return state
+}
+
+export const useDispatchContext = () => {
+  const dispatch = useContext(dispatchContext)
+  if (!dispatch) {
+    throw new Error('require wrapped by stateContext')
+  }
+  return dispatch
+}
+
+export default StateContextProvider
--- webapp/frontend/src/main.tsx
+++ webapp/frontend/src/main.tsx
@@ -6,19 +6,22 @@ import App from './App'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import GuardedRoute from './router/GuardedRoute'
 import Auth from './pages/Auth'
+import StateContextProvider from './context/state'
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <GuardedRoute path="/" exact>
-          <App />
-        </GuardedRoute>
-        <Route path="/login">
-          <Auth />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <StateContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <GuardedRoute path="/" exact>
+            <App />
+          </GuardedRoute>
+          <Route path="/login">
+            <Auth />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </StateContextProvider>
   </StrictMode>,
   document.getElementById('root')
 )
