a31221bc652d7d87a7b8bbeb3e4a111b800a793c
ryoha000 <the7uya@gmail.com>
2021-06-18 10:24:51 +0900

    feat(frontend) controlを整理

--- webapp/frontend/src/components/PageHeader/Controls.tsx
+++ webapp/frontend/src/components/PageHeader/Controls.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useStateContext } from '../../context/state'
+import { MdAccountCircle } from 'react-icons/md'
+import ControlItem from './ControlItem'
 
 const Controls = () => {
   const me = useStateContext().me
@@ -7,14 +9,23 @@ const Controls = () => {
     return <></>
   }
   return (
-    <div className="flex ml-auto">
-      <Link to="/isu/condition">
-        <div>ISUの状態</div>
-      </Link>
-      <Link to="/isu/search">
-        <div>ISUの検索</div>
-      </Link>
-      <div>{me.jiaUserID}</div>
+    <div className="flex ml-auto items-center">
+      <ControlItem>
+        <Link to="/isu/condition">
+          <div>ISUの状態</div>
+        </Link>
+      </ControlItem>
+      <ControlItem>
+        <Link to="/isu/search">
+          <div>ISUの検索</div>
+        </Link>
+      </ControlItem>
+      <ControlItem>
+        <div className="flex items-center cursor-pointer">
+          <MdAccountCircle />
+          <div>{me.jiaUserID}</div>
+        </div>
+      </ControlItem>
     </div>
   )
 }
--- webapp/frontend/src/components/PageHeader/PageHeader.tsx
+++ webapp/frontend/src/components/PageHeader/PageHeader.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import Controls from './Controls'
 import logo from '/@/assets/logo.png'
 
 const PageHeader = () => {
   return (
     <header className="flex p-2 items-center bg-primary-400">
-      <img src={logo} alt="isucondition" />
+      <Link to="/">
+        <img src={logo} alt="isucondition" className="cursor-pointer" />
+      </Link>
       <Controls />
     </header>
   )
