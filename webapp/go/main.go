5863e056a882fff76cf397bd640cfb030992955f
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2021-06-22 15:38:30 +0900

    fix(backend) 型の不一致を修正

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -717,7 +717,12 @@ func deleteIsu(c echo.Context) error {
 
 	// JIAにisuのdeactivateをリクエスト
 	targetURL := fmt.Sprintf("%s/api/deactivate", getJIAServiceURL())
-	body := JIAServiceRequest{DefaultIsuConditionHost, getEnv("SERVER_PORT", "3000"), jiaIsuUUID}
+	port, err := strconv.Atoi(getEnv("SERVER_PORT", "3000"))
+	if err != nil {
+		c.Logger().Errorf("bad port number: %v", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	body := JIAServiceRequest{DefaultIsuConditionHost, port, jiaIsuUUID}
 	bodyJSON, err := json.Marshal(body)
 	if err != nil {
 		c.Logger().Errorf("failed to marshal data: %v", err)
