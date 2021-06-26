326a812193680b29575ee09cae48e85b7856e094
ryoha000 <the7uya@gmail.com>
2021-06-27 03:31:50 +0900

    fix(frontend) デバッグ用の記述を削除

--- webapp/frontend/src/pages/Auth.tsx
+++ webapp/frontend/src/pages/Auth.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import { useDispatchContext } from '../context/state'
-import apis, { debugGetJWT, Isu } from '../lib/apis'
+import apis, { debugGetJWT } from '../lib/apis'
 
 const Auth = () => {
   const dispatch = useDispatchContext()
-  const [isus, setIsus] = useState<Isu[]>([])
 
   // TODO: 外部APIのログインページにリダイレクト
   const click = async () => {
@@ -13,23 +10,11 @@ const Auth = () => {
     await apis.postAuth(jwt)
 
     const me = await apis.getUserMe()
-    setIsus(await apis.getIsus())
     dispatch({ type: 'login', user: me })
   }
   return (
     <div>
       <button onClick={click}>ISU協会でログイン</button>
-      {isus.map(isu => {
-        return (
-          <Link
-            to={`/isu/${isu.jia_isu_uuid}`}
-            key={isu.jia_isu_uuid}
-            className="mr-2"
-          >
-            {isu.name}
-          </Link>
-        )
-      })}
     </div>
   )
 }
