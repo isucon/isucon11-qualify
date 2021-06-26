f53ed2cc803026ffcaa5ef838f3e7874c7474b4f
ryoha000 <the7uya@gmail.com>
2021-06-26 21:44:32 +0900

    feat(frontend) 登録画面を作成

--- webapp/frontend/src/pages/Register.tsx
+++ webapp/frontend/src/pages/Register.tsx
@@ -1,5 +1,26 @@
+import { FormEvent, useState } from 'react'
+import Input from '../components/UI/Input'
+import apis from '../lib/apis'
+
 const Register = () => {
-  return <div>ISUを登録</div>
+  const [id, setId] = useState('')
+  const [name, setName] = useState('')
+  const submit = (event: FormEvent<HTMLFormElement>) => {
+    apis.postIsu({ jia_isu_uuid: id, isu_name: name })
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <Input label={'JIAのIsuID'} value={id} setValue={setId}></Input>
+        <Input label={'ISUの名前'} value={name} setValue={setName}></Input>
+        <button type="submit" className="border-dark-200 border">
+          登録
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default Register
