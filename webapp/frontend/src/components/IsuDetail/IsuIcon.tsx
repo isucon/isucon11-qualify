95f1b11451ebfb740ffbe4155b8a2f1b462ba296
ryoha000 <the7uya@gmail.com>
2021-06-29 05:22:22 +0900

    feat(frontend) IconInputのinterfaceの変更とスタイル付け

--- webapp/frontend/src/components/IsuDetail/IsuIcon.tsx
+++ webapp/frontend/src/components/IsuDetail/IsuIcon.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
 import { Isu } from '../../lib/apis'
-import IconInput from '../UI/IconInput'
 
-const IsuIcon = ({ isu }: { isu: Isu }) => {
-  const [key, setKey] = useState(0)
-  const reloadIcon = () => {
-    // 画像をアップデートしてもsrcとかが更新されるわけではないので再レンダリングのためセットしている
-    setKey(performance.now())
-  }
-
+interface Props {
+  isu: Isu
+  reloadKey?: number
+}
+const IsuIcon = ({ isu, reloadKey }: Props) => {
   return (
     <div>
-      <img src={`/api/isu/${isu.jia_isu_uuid}/icon`} key={key} />
-      <IconInput isu={isu} reloadIcon={reloadIcon} />
+      <img src={`/api/isu/${isu.jia_isu_uuid}/icon`} key={reloadKey} />
     </div>
   )
 }
