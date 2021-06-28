d00399813f80a8d542f58b723c479e53af315e4a
ryoha000 <the7uya@gmail.com>
2021-06-29 05:01:38 +0900

    feat(frontend) 背景の調整

--- webapp/frontend/src/App.tsx
+++ webapp/frontend/src/App.tsx
@@ -6,24 +6,26 @@ import GuardedRoute from './router/GuardedRoute'
 
 const App = () => {
   return (
-    <div className="text-primary">
+    <div className="flex flex-col min-h-full text-primary">
       <BrowserRouter>
         <PageHeader></PageHeader>
-        <Switch>
-          <GuardedRoute path="/" exact></GuardedRoute>
-          <GuardedRoute path="/condition" exact>
-            <div>通知画面</div>
-          </GuardedRoute>
-          <GuardedRoute path="/search" exact>
-            <div>検索画面</div>
-          </GuardedRoute>
-          <GuardedRoute path="/isu/:id">
-            <IsuRoot />
-          </GuardedRoute>
-          <Route path="/login">
-            <Auth />
-          </Route>
-        </Switch>
+        <div className="flex-1 bg-teritary">
+          <Switch>
+            <GuardedRoute path="/" exact></GuardedRoute>
+            <GuardedRoute path="/condition" exact>
+              <div>通知画面</div>
+            </GuardedRoute>
+            <GuardedRoute path="/search" exact>
+              <div>検索画面</div>
+            </GuardedRoute>
+            <GuardedRoute path="/isu/:id">
+              <IsuRoot />
+            </GuardedRoute>
+            <Route path="/login">
+              <Auth />
+            </Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   )
