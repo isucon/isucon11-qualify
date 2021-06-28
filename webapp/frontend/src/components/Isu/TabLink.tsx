260808552429aa266c2aff76b75b867f7c4f611b
ryoha000 <the7uya@gmail.com>
2021-06-29 04:43:55 +0900

    feat(frontend) タブのスタイル当て

---
+++ webapp/frontend/src/components/Isu/TabLink.tsx
@@ -0,0 +1,25 @@
+import { Link, useLocation } from 'react-router-dom'
+
+interface Props {
+  to: string
+  label: string
+}
+const TabLink = ({ to, label }: Props) => {
+  const { pathname } = useLocation()
+
+  // TODO: これだとtrailing slashが判別できないからすべきか含めて考える
+  const isSelected = pathname === to
+  return (
+    <div
+      className={`w-16 flex justify-center ${
+        isSelected
+          ? 'border-b-2 text-accent-primary border-accent-primary'
+          : 'text-secondary'
+      }`}
+    >
+      <Link to={to}>{label}</Link>
+    </div>
+  )
+}
+
+export default TabLink
