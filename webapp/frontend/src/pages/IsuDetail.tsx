bffe73a92d45085c20a9f1385fb2766e55b540c0
ryoha000 <the7uya@gmail.com>
2021-06-29 07:28:00 +0900

    Catalogコンポーネントを追加

--- webapp/frontend/src/pages/IsuDetail.tsx
+++ webapp/frontend/src/pages/IsuDetail.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import CatalogInfo from '../components/IsuDetail/Catalog'
 import MainInfo from '../components/IsuDetail/MainInfo'
 import Card from '../components/UI/Card'
@@ -15,14 +14,13 @@ const IsuDetail = ({ isu, setIsu }: Props) => {
     return <NowLoading />
   }
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col gap-10 items-center">
       <Card>
         <MainInfo isu={isu} setIsu={setIsu} />
       </Card>
-      <div>椅子詳細</div>
-      <CatalogInfo isu={isu} />
-      <Link to={`/isu/${isu.jia_isu_uuid}/graph`}>グラフの確認</Link>
-      <Link to={`/isu/${isu.jia_isu_uuid}/condition`}>状態の確認</Link>
+      <Card>
+        <CatalogInfo isu={isu} />
+      </Card>
     </div>
   )
 }
