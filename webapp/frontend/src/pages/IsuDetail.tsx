c6796533afd0b147bd90f1ce72adb9588ff834a1
ryoha000 <the7uya@gmail.com>
2021-06-29 05:38:09 +0900

    feat(frontend) IsuDetailにあったロジックを削除

--- webapp/frontend/src/pages/IsuDetail.tsx
+++ webapp/frontend/src/pages/IsuDetail.tsx
@@ -1,31 +1,23 @@
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import CatalogInfo from '../components/IsuDetail/Catalog'
-import IsuIcon from '../components/IsuDetail/IsuIcon'
+import MainInfo from '../components/IsuDetail/MainInfo'
+import Card from '../components/UI/Card'
 import NowLoading from '../components/UI/NowLoading'
-import apis, { Isu } from '../lib/apis'
+import { Isu } from '../lib/apis'
 
 const IsuDetail = ({ isu }: { isu: Isu }) => {
-  const history = useHistory()
-
-  const deleteIsu = async () => {
-    if (isu && confirm(`本当に${isu.name}を削除しますか？`)) {
-      await apis.deleteIsu(isu.jia_isu_uuid)
-      history.push('/')
-    }
-  }
-
   if (!isu) {
     return <NowLoading />
   }
   return (
-    <div>
+    <div className="flex flex-col items-center">
+      <Card>
+        <MainInfo isu={isu} />
+      </Card>
       <div>椅子詳細</div>
-      <div>{isu.name}</div>
       <CatalogInfo isu={isu} />
-      <IsuIcon isu={isu} />
       <Link to={`/isu/${isu.jia_isu_uuid}/graph`}>グラフの確認</Link>
       <Link to={`/isu/${isu.jia_isu_uuid}/condition`}>状態の確認</Link>
-      <button onClick={deleteIsu}>削除</button>
     </div>
   )
 }
