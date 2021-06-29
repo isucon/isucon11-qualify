3d9f6cca129eb2a637bf4c29df2a69a4e687b194
Nagarei <Nagarei@users.noreply.github.com>
2021-06-29 22:56:34 +0900

    fix(bench): スレッド間通信の整理
    #ref 188

--- bench/model/isu.go
+++ bench/model/isu.go
@@ -1,9 +1,10 @@
 package model
 
 import (
-	"context"
 	"sync"
 )
+
+type IsuStateChange int
 
 type IsuConditionArray struct {
 	Mutex      sync.Mutex
@@ -11,25 +12,39 @@ type IsuConditionArray struct {
 }
 
 //一つのIsuにつき、一つの送信用スレッドがある
+//IsuはISU協会スレッドからも読み込まれる
 type Isu struct {
-	Owner          *User
-	JIAIsuUUID     string             `json:"jia_isu_uuid"`
-	Name           string             `json:"name"`
-	ImageName      string             `json:"-"`
-	JIACatalogID   string             `json:"jia_catalog_id"`
-	Character      string             `json:"character"`
-	isWantDeleted  bool               //シナリオスレッドからのみ参照
-	deactivateFunc context.CancelFunc //Isu協会activate/deactivateスレッドからのみ参照
-	//TODO: その他postスレッドと通信するためのchannel //シナリオスレッド->postingスレッド
-	Conditions *IsuConditionArray //シナリオスレッドからread、postingスレッドからwrite
+	Owner         *User
+	JIAIsuUUID    string `json:"jia_isu_uuid"`
+	Name          string `json:"name"`
+	ImageName     string `json:"-"`
+	JIACatalogID  string `json:"jia_catalog_id"`
+	Character     string `json:"character"`
+	isWantDeleted bool   //シナリオスレッドからのみ参照
+	isDeactivated bool
+	activateChan  chan bool //Isu協会 -> シナリオスレッド
+	//deactivateFunc context.CancelFunc //Isu協会activate/deactivateスレッドからのみ参照 //TODO: Isu協会側でデータを持つ
+	isuChan    chan IsuStateChange //シナリオスレッド->postingスレッド
+	Conditions *IsuConditionArray  //シナリオスレッドからread、postingスレッドからwrite
 }
 
-//Isu協会activate/deactivateスレッドからのみ呼び出される
-func (isu *Isu) Deactivate() {
-	if isu.deactivateFunc != nil {
-		isu.deactivateFunc()
-		isu.deactivateFunc = nil
+func NewIsu() *Isu {
+	v := &Isu{
+		isDeactivated: true,
+		//TODO: ポインタやchanの初期化
 	}
+	//TODO: ISU協会にIsu*を登録
+
+	return v
+}
+
+func (isu *Isu) IsDeactivated() bool {
+	select {
+	case v, ok := <-isu.activateChan:
+		isu.isDeactivated = !ok || !v //Isu協会スレッドの終了 || deactivateされた
+	default:
+	}
+	return isu.isDeactivated
 }
 
 //シナリオスレッドからのみ参照
