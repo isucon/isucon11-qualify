322e733744e56b6721a1cfd283d4ea3894de39a3
ryoha000 <the7uya@gmail.com>
2021-06-29 15:45:31 +0900

    fix(frontend) 汎用的なinputのスタイル調整

--- webapp/frontend/src/components/UI/Input.tsx
+++ webapp/frontend/src/components/UI/Input.tsx
@@ -2,19 +2,21 @@ interface Props {
   label: string
   value: string
   setValue: (newValue: string) => void
+  classname?: string
 }
 
 const Input = <T extends Props>({
   label,
   value,
   setValue,
+  classname,
   ...inputProps
 }: T) => {
   return (
-    <label>
+    <label className={'flex flex-col ' + classname}>
       {label}
       <input
-        className="border-dark-200 border"
+        className="p-1 bg-teritary border-2 border-outline"
         value={value}
         onChange={e => setValue(e.target.value)}
         {...inputProps}
