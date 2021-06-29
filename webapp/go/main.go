78e35997ef96e1b5dab0b894dadc92e865dfada5
mofumofuchan <you_oki1@yahoo.co.jp>
2021-06-29 20:38:21 +0900

    fix(backend) 不要なコメントを削除

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -156,8 +156,8 @@ type PutIsuRequest struct {
 }
 
 type GraphResponse struct {
-	StartAt int64      `json:"start_at"` // FIXME
-	EndAt   int64      `json:"end_at"`   // FIXME
+	StartAt int64      `json:"start_at"`
+	EndAt   int64      `json:"end_at"`
 	Data    *GraphData `json:"data"`
 }
 
