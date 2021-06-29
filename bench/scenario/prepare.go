104acb5d00bb194323dc942ceca5e69d5d73f801
Nagarei <Nagarei@users.noreply.github.com>
2021-06-29 10:38:16 +0900

    feat(bench): 作成済みユーザーへのログイン確認

--- bench/scenario/prepare.go
+++ bench/scenario/prepare.go
@@ -57,7 +57,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 
 	w, err := worker.NewWorker(func(ctx context.Context, index int) {
 
-		a, err := s.NewAgent()
+		agt, err := s.NewAgent()
 		if err != nil {
 			step.AddError(failure.NewError(ErrCritical, err))
 			return
@@ -70,7 +70,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 		switch index {
 		default:
 			//ログイン成功
-			_, errs := authAction(ctx, a, userID)
+			_, errs := authAction(ctx, agt, userID)
 			for _, err := range errs {
 				step.AddError(err)
 			}
@@ -81,7 +81,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 				step.AddError(failure.NewError(ErrCritical, err))
 				return
 			}
-			errs := authActionWithForbiddenJWT(ctx, a, jwtHS256)
+			errs := authActionWithForbiddenJWT(ctx, agt, jwtHS256)
 			for _, err := range errs {
 				step.AddError(err)
 			}
@@ -92,7 +92,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 				step.AddError(failure.NewError(ErrCritical, err))
 				return
 			}
-			errs := authActionWithForbiddenJWT(ctx, a, jwtExpired)
+			errs := authActionWithForbiddenJWT(ctx, agt, jwtExpired)
 			for _, err := range errs {
 				step.AddError(err)
 			}
@@ -106,7 +106,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 				step.AddError(failure.NewError(ErrCritical, err))
 				return
 			}
-			errs := authActionWithForbiddenJWT(ctx, a, jwtDummyKey)
+			errs := authActionWithForbiddenJWT(ctx, agt, jwtDummyKey)
 			for _, err := range errs {
 				step.AddError(err)
 			}
@@ -115,7 +115,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 			//TODO:
 		case 6:
 			//not jwt, StatusForbidden
-			errs := authActionWithForbiddenJWT(ctx, a, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.")
+			errs := authActionWithForbiddenJWT(ctx, agt, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.")
 			for _, err := range errs {
 				step.AddError(err)
 			}
@@ -131,7 +131,7 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 				step.AddError(failure.NewError(ErrCritical, err))
 				return
 			}
-			errs := authActionWithForbiddenJWT(ctx, a, jwtTampered)
+			errs := authActionWithForbiddenJWT(ctx, agt, jwtTampered)
 			for _, err := range errs {
 				step.AddError(err)
 			}
@@ -147,7 +147,27 @@ func (s *Scenario) prepareCheckAuth(ctx context.Context, step *isucandar.Benchma
 	//w.Wait() //念のためもう一度止まってるか確認
 
 	//作成済みユーザーへのログイン確認
-	//TODO:
+	agt, err := s.NewAgent()
+	if err != nil {
+		step.AddError(failure.NewError(ErrCritical, err))
+		return nil
+	}
+	userID, err := model.MakeRandomUserID()
+	if err != nil {
+		step.AddError(failure.NewError(ErrCritical, err))
+		return nil
+	}
+
+	_, errs := authAction(ctx, agt, userID)
+	for _, err := range errs {
+		step.AddError(err)
+	}
+	agt.ClearCookie()
+	//二回目のログイン
+	_, errs = authAction(ctx, agt, userID)
+	for _, err := range errs {
+		step.AddError(err)
+	}
 
 	return nil
 }
