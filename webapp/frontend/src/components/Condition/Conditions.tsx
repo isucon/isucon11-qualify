f1dee0e8d6cf782ef30c7cec77d1a3f6fc018da7
ryoha000 <the7uya@gmail.com>
2021-06-29 17:48:13 +0900

    feat(frontend) ページングのstyleを実装

--- webapp/frontend/src/components/Condition/Conditions.tsx
+++ webapp/frontend/src/components/Condition/Conditions.tsx
@@ -1,18 +1,33 @@
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 import { Condition } from '../../lib/apis'
+import IconButton from '../UI/IconButton'
 import ConditionDetail from './ConditionDetail'
 
 interface Props {
   conditions: Condition[]
 }
 
+const DEFAULT_CONDITION_LIMIT = 20
+
 const Conditions = ({ conditions }: Props) => {
   return (
-    <div className="border border-b-0 border-outline">
-      {conditions.map((condition, i) => (
-        <div className="border-b border-outline" key={i}>
-          <ConditionDetail condition={condition} />
-        </div>
-      ))}
+    <div className="flex flex-col gap-4 items-center">
+      <div className="w-full border border-b-0 border-outline">
+        {conditions.map((condition, i) => (
+          <div className="border-b border-outline" key={i}>
+            <ConditionDetail condition={condition} />
+          </div>
+        ))}
+      </div>
+      <div className="center flex gap-8">
+        <IconButton>
+          <IoIosArrowBack size={24} />
+        </IconButton>
+        <div className="align-middle text-xl">1</div>
+        <IconButton>
+          <IoIosArrowForward size={24} />
+        </IconButton>
+      </div>
     </div>
   )
 }
