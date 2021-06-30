325185120eba7c70a0f3c634e3ecec20930b0074
ryoha000 <the7uya@gmail.com>
2021-06-30 13:52:51 +0900

    feat(frontend) ButtonがButtonのpropsを受け取るように

--- webapp/frontend/src/components/UI/Button.tsx
+++ webapp/frontend/src/components/UI/Button.tsx
@@ -1,14 +1,20 @@
+import { DetailedHTMLProps, ButtonHTMLAttributes } from "react"
+
 interface Props {
   label: string
   classname?: string
 }
-const Button = ({ label, classname }: Props) => {
+
+type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+const Button = ({ label, classname, ...buttonProps }: Props & ButtonProps) => {
   return (
     <button
       className={
         'px-3 py-1 h-8 leading-4 border border-outline rounded focus:outline-none ' +
         classname
       }
+      {...buttonProps}
     >
       {label}
     </button>
