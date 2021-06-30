a495cbc28e1fba485bc42a80f2d1f48c311b3651
ryoha000 <the7uya@gmail.com>
2021-07-01 02:38:04 +0900

    feat(frontend) 検索ロジックを追加

--- webapp/frontend/src/components/Search/use/search.ts
+++ webapp/frontend/src/components/Search/use/search.ts
@@ -1,10 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import apis, { Isu } from '../../../lib/apis'
+import parseQuery from './parseQuery'
 
 const useSearch = () => {
   const [query, setQuery] = useState('')
-  const [page, usePage] = useState(1)
+  const [page, setPage] = useState(1)
+  const [isus, setIsus] = useState<Isu[]>([])
+  useEffect(() => {
+    const fetchIsus = async () => {
+      setIsus(await apis.getIsuSearch())
+    }
+    fetchIsus()
+  }, [setIsus])
 
-  return { query, setQuery, page }
+  const search = async (newQuery: string) => {
+    const params = parseQuery(newQuery)
+    params.page = '1'
+    setIsus(await apis.getIsuSearch(params))
+    setQuery(query)
+  }
+  const next = async () => {
+    const params = parseQuery(query)
+    params.page = `${page + 1}`
+    setIsus(await apis.getIsuSearch(params))
+    setPage(page + 1)
+  }
+  const prev = async () => {
+    const params = parseQuery(query)
+    params.page = `${page - 1}`
+    setIsus(await apis.getIsuSearch(params))
+    setPage(page - 1)
+  }
+  return { query, search, isus, next, prev, page }
 }
 
 export default useSearch
