a1767d9f6b6a12da191b818240d290354daa4a87
ryoha000 <the7uya@gmail.com>
2021-06-30 23:56:30 +0900

    feat(frontend) Isuコンポーネントの実装

---
+++ webapp/frontend/src/components/UI/Isu.tsx
@@ -0,0 +1,20 @@
+import { Isu as IIsu } from '../../lib/apis'
+
+interface Props {
+  isu: IIsu
+}
+
+const Isu = ({ isu }: Props) => {
+  return (
+    <div className="flex flex-col items-center">
+      <img
+        src={`/api/isu/${isu.jia_isu_uuid}/icon`}
+        className="w-48 h-48 object-contain"
+        key={isu.jia_isu_uuid}
+      />
+      <h3>{isu.name}</h3>
+    </div>
+  )
+}
+
+export default Isu
