f1afdbdb62d99164130b5f8f296319b8b3e2cb63
ryoha000 <the7uya@gmail.com>
2021-07-01 05:55:13 +0900

    feat(frontend) カーソルの挿入位置を、keyがすでにあるならそのvalueの一番最後にする

--- webapp/frontend/src/components/Search/use/insertQuery.ts
+++ webapp/frontend/src/components/Search/use/insertQuery.ts
@@ -1,4 +1,5 @@
-import { Dispatch, RefObject, SetStateAction } from 'react'
+import { Dispatch, RefObject, SetStateAction, useCallback } from 'react'
+import { parseQuery } from './parseQuery'
 
 const useInsertQuery = (
   inputRef: RefObject<HTMLInputElement>,
@@ -7,18 +8,28 @@ const useInsertQuery = (
 ) => {
   const insert = (key: string) => {
     if (inputRef.current) {
-      const newQuery = `${query} ${key}:""`
-      inputRef.current.value = newQuery
-      inputRef.current.focus()
-      inputRef.current.setSelectionRange(
-        newQuery.length - 1,
-        newQuery.length - 1
-      )
-      setQuery(newQuery)
+      const params = parseQuery(query)
+      if (params[key]) {
+        setCursor(params[key].valueNextIndex)
+      } else {
+        const newQuery = `${query} ${key}:""`
+        inputRef.current.value = newQuery
+        setCursor(newQuery.length - 1)
+        setQuery(newQuery)
+      }
     } else {
       throw 'inputRef.current is falthy'
     }
   }
+  const setCursor = useCallback(
+    (insertIndex: number) => {
+      if (inputRef.current) {
+        inputRef.current.focus()
+        inputRef.current.setSelectionRange(insertIndex, insertIndex)
+      }
+    },
+    [inputRef]
+  )
   return { insert }
 }
 
