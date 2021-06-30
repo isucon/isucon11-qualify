ccd664316079d8b074f7c451e4d61e81c33aa9e4
ryoha000 <the7uya@gmail.com>
2021-06-30 20:51:33 +0900

    feat(frontend) HomeConditionの追加

--- webapp/frontend/src/pages/Home.tsx
+++ webapp/frontend/src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import HomeCondition from '../components/Home/Condition'
+import Card from '../components/UI/Card'
+
 const Home = () => {
   return (
-    <div>
-      <div>Home</div>
+    <div className="flex flex-col gap-10 items-center p-10">
+      <Card>
+        <div>aikonntoka</div>
+      </Card>
+      <Card>
+        <HomeCondition />
+      </Card>
     </div>
   )
 }
