7b0c6f9977f09d7cb295cdc1963bc3b1c906cd9f
ryoha000 <the7uya@gmail.com>
2021-07-01 09:43:37 +0900

    feat(frontend) ログイン機能の実装

--- webapp/frontend/src/pages/Auth.tsx
+++ webapp/frontend/src/pages/Auth.tsx
@@ -1,17 +1,27 @@
 import { useDispatchContext } from '../context/state'
-import apis, { debugGetJWT } from '../lib/apis'
+import apis from '../lib/apis'
+import { useEffect } from 'react'
 
 const Auth = () => {
   const dispatch = useDispatchContext()
+  useEffect(() => {
+    const login = async () => {
+      const url = new URL(location.href)
+      const jwt = url.searchParams.get('jwt')
+      if (jwt) {
+        await apis.postAuth(jwt)
 
-  // TODO: 外部APIのログインページにリダイレクト
+        const me = await apis.getUserMe()
+        dispatch({ type: 'login', user: me })
+      }
+    }
+    login()
+  })
   const click = async () => {
-    const jwt = await debugGetJWT()
-    await apis.postAuth(jwt)
-
-    const me = await apis.getUserMe()
-    dispatch({ type: 'login', user: me })
+    // TODO: 本番どうするか考える
+    location.href = `http://localhost:5000`
   }
+
   return (
     <div>
       <button onClick={click}>ISU協会でログイン</button>
