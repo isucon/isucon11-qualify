f049cdccca05dab8f3fb9db27c40416ab08a306f
Nagarei <Nagarei@users.noreply.github.com>
2021-07-02 12:37:36 +0900

    fix(bench): チャネルを分離

--- bench/model/isu.go
+++ bench/model/isu.go
@@ -6,26 +6,34 @@ type IsuStateChange int
 //posterスレッドとシナリオスレッドとの通信に必要な情報
 //複数回posterスレッドが起動するかもしれないのでcloseしない
 //当然リソースリークするがベンチマーカーは毎回落とすので問題ない
-type IsuPosterChan struct {
-	JIAIsuUUID    string
-	activateChan  chan bool           //posterスレッド -> シナリオスレッド
-	isuChan       chan IsuStateChange //シナリオスレッド->posterスレッド
-	conditionChan chan IsuCondition   //posterスレッド -> シナリオスレッド
+type StreamsForPoster struct {
+	ActiveChan    chan<- bool
+	StateChan     <-chan IsuStateChange
+	ConditionChan chan<- IsuCondition
+}
+
+//posterスレッドとシナリオスレッドとの通信に必要な情報
+//複数回posterスレッドが起動するかもしれないのでcloseしない
+//当然リソースリークするがベンチマーカーは毎回落とすので問題ない
+type StreamsForScenario struct {
+	activeChan    <-chan bool
+	StateChan     chan<- IsuStateChange
+	ConditionChan <-chan IsuCondition
 }
 
 //一つのIsuにつき、一つの送信用スレッドがある
 //IsuはISU協会スレッドからも読み込まれる
 type Isu struct {
-	Owner             *User
-	JIAIsuUUID        string
-	Name              string
-	ImageName         string
-	JIACatalogID      string
-	Character         string
-	IsWantDeactivated bool           //シナリオ上でDeleteリクエストを送ったかどうか
-	isDeactivated     bool           //実際にdeactivateされているか
-	PosterChan        *IsuPosterChan //ISU協会はこれを使ってposterスレッドを起動、posterスレッドはこれを使って通信
-	Conditions        []IsuCondition //シナリオスレッドからのみ参照
+	Owner              *User
+	JIAIsuUUID         string
+	Name               string
+	ImageName          string
+	JIACatalogID       string
+	Character          string
+	IsWantDeactivated  bool                //シナリオ上でDeleteリクエストを送ったかどうか
+	isDeactivated      bool                //実際にdeactivateされているか
+	StreamsForScenario *StreamsForScenario //ISU協会はこれを使ってposterスレッドを起動、posterスレッドはこれを使って通信
+	Conditions         []IsuCondition      //シナリオスレッドからのみ参照
 }
 
 func NewIsu() *Isu {
@@ -41,7 +49,7 @@ func NewIsu() *Isu {
 //シナリオスレッドからのみ参照
 func (isu *Isu) IsDeactivated() bool {
 	select {
-	case v, ok := <-isu.PosterChan.activateChan:
+	case v, ok := <-isu.StreamsForScenario.activeChan:
 		isu.isDeactivated = !ok || !v //Isu協会スレッドの終了 || deactivateされた
 	default:
 	}
