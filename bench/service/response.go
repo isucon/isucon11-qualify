4a17b7dbb57b793cc5ac1121d33f16a8af2f477d
ryoha000 <the7uya@gmail.com>
2021-07-03 05:24:42 +0900

    feat(bench) 構造体の追加

--- bench/service/response.go
+++ bench/service/response.go
@@ -12,3 +12,42 @@ type SignoutResponse struct {
 type GetMeResponse struct {
 	JIAUserID string `json:"jia_user_id"`
 }
+
+type Isu struct {
+	JIAIsuUUID   string `json:"jia_isu_uuid"`
+	Name         string `json:"name"`
+	JIACatalogID string `json:"jia_catalog_id"`
+	Character    string `json:"character"`
+}
+
+type Catalog struct {
+	JIACatalogID string `json:"jia_catalog_id"`
+	Name         string `json:"name"`
+	LimitWeight  int    `json:"limit_weight"`
+	Weight       int    `json:"weight"`
+	Size         string `json:"size"`
+	Maker        string `json:"maker"`
+	Tags         string `json:"tags"`
+}
+
+type GetIsuConditionResponse struct {
+	JIAIsuUUID     string `json:"jia_isu_uuid"`
+	IsuName        string `json:"isu_name"`
+	Timestamp      int64  `json:"timestamp"`
+	IsSitting      bool   `json:"is_sitting"`
+	Condition      string `json:"condition"`
+	ConditionLevel string `json:"condition_level"`
+	Message        string `json:"message"`
+}
+
+type GraphResponse struct {
+	StartAt int64      `json:"start_at"`
+	EndAt   int64      `json:"end_at"`
+	Data    *GraphData `json:"data"`
+}
+
+type GraphData struct {
+	Score   int            `json:"score"`
+	Sitting int            `json:"sitting"`
+	Detail  map[string]int `json:"detail"`
+}
