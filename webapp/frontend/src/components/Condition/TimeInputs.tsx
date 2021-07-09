21b5377fca514ab95e840d5977aedade9698fd16
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2021-07-09 22:42:58 +0900

    feat(frontend): inputのスタイルを変更

--- webapp/frontend/src/components/Condition/TimeInputs.tsx
+++ webapp/frontend/src/components/Condition/TimeInputs.tsx
@@ -9,14 +9,14 @@ const TimeInputs = ({ times, setTimes }: Props) => {
       時間指定
       <div className="flex items-center">
         <input
-          className="p-1 w-40 bg-teritary border-2 border-outline rounded"
+          className="px-2 py-1 w-40 bg-teritary border-2 border-outline rounded"
           value={times[0]}
           onChange={e => setTimes([e.target.value, times[1]])}
           placeholder={'2020/01/01 11:11:11'}
         ></input>
         <div className="text-xl">~</div>
         <input
-          className="p-1 w-40 bg-teritary border-2 border-outline rounded"
+          className="px-2 py-1 w-40 bg-teritary border-2 border-outline rounded"
           value={times[1]}
           onChange={e => setTimes([times[0], e.target.value])}
           placeholder={'2020/01/01 11:11:11'}
