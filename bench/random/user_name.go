2d820ad27180f1315eb4543db4021c8448d1ca86
ShotaKitazawa <skitazawa1121@gmail.com>
2021-07-22 02:26:11 +0900

    feat(bench): ランダムなユーザ名の作成関数にて無限ループが回ってしまっている場合に警告メッセージを標準出力するようにした

--- bench/random/user_name.go
+++ bench/random/user_name.go
@@ -1,9 +1,14 @@
 package random
 
 import (
+	"log"
 	"sync"
 
 	"github.com/docker/docker/pkg/namesgenerator"
+)
+
+const (
+	retry = 10000
 )
 
 var (
@@ -15,12 +20,16 @@ func init() {
 	generatedUser = make(map[string]struct{}, 128)
 }
 
+// 108 * 237 通りのユーザ名を重複なしで返す
 func UserName() string {
 	var username string
-	for { // bench内から呼び出す処理で log.Fatalf して欲しくないので、無限ループする
+	for i := 0; true; i++ { // bench内から呼び出す処理で log.Fatalf して欲しくないので、無限ループする
 		username = namesgenerator.GetRandomName(0)
 		if !hasAlreadyGenerated(username) {
 			break
+		}
+		if i == retry-1 {
+			log.Printf("[WARNING] username generating is probably in an infinite loop: already retried %d times\n", retry)
 		}
 	}
 	setGeneratedUser(username)
