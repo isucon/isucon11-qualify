8fbf8aee6082f36879894ae3856c1a4dd6930b6f
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2021-07-27 21:00:52 +0900

    feat(frontend): png画像を受け付けないようにした

--- webapp/frontend/src/components/UI/IconInput.tsx
+++ webapp/frontend/src/components/UI/IconInput.tsx
@@ -7,7 +7,7 @@ interface Props {
 const useImageSelect = (onSelect: (file: File) => void) => {
   const input = document.createElement('input')
   input.type = 'file'
-  input.accept = 'image/png,image/jpeg'
+  input.accept = 'image/jpeg'
 
   const onChange = () => {
     if (input.files && input.files[0]) {
