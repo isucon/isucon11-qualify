c49d6ec0a93a0bc1e2114d22814a2334d72a7025
yotabuch <yotabuch@yahoo-corp.jp>
2021-08-01 22:53:59 +0900

    jsonデータ用に修正

--- extra/initial-data/models/json.go
+++ extra/initial-data/models/json.go
@@ -29,17 +29,19 @@ type Json struct {
 type IsuListById map[string]JsonIsuInfo
 
 type JsonIsuInfo struct {
+	Id            int            `json:"id"`
 	Name          string         `json:"name"`
-	ImageFileHash string         `json:"image_file_hash"`
+	ImageFileHash [md5.Size]byte `json:"image_file_hash"`
 	Character     string         `json:"character"`
 	Conditions    JsonConditions `json:"conditions"`
 	CreatedAt     time.Time      `json:"created_at"`
 }
 
-func ToJsonIsuInfo(isu Isu, conditions JsonConditions) JsonIsuInfo {
+func ToJsonIsuInfo(id int, isu Isu, conditions JsonConditions) JsonIsuInfo {
 	return JsonIsuInfo{
+		id,
 		isu.Name,
-		fmt.Sprintf("%x", md5.Sum(isu.Image)),
+		md5.Sum(isu.Image),
 		isu.Character,
 		conditions,
 		isu.CreatedAt,
@@ -53,24 +55,33 @@ type JsonConditions struct {
 }
 
 type JsonCondition struct {
-	Timestamp    int64     `json:"timestamp"`
-	IsSitting    bool      `json:"is_sitting"`
-	IsDirty      bool      `json:"is_dirty"`
-	IsOverweight bool      `json:"is_overweight"`
-	IsBroken     bool      `json:"is_broken"`
-	Message      string    `json:"message"`
-	CreatedAt    time.Time `json:"created_at"`
+	Timestamp      int64          `json:"timestamp"`
+	IsSitting      bool           `json:"is_sitting"`
+	IsDirty        bool           `json:"is_dirty"`
+	IsOverweight   bool           `json:"is_overweight"`
+	IsBroken       bool           `json:"is_broken"`
+	Message        string         `json:"message"`
+	CreatedAt      time.Time      `json:"created_at"`
+	OwnerIsuUUID   string         `json:"owner_isu_uuid"`
+	OwnerIsuID     int            `json:"owner_isu_id"`
+	ConditionLevel ConditionLevel `json:"condition_level"`
 }
 
-func (j *JsonConditions) AddCondition(condition Condition) error {
+func (j *JsonConditions) AddCondition(condition Condition, isuId int) error {
 	jsonCondition := JsonCondition{
-		condition.Timestamp.Unix(),
+		// JST分マイナスすると何故かちょうどよい
+		// DB上はJSTなのでUnixtimeはJST時間に変換されている（UTC時間+9表記）
+		// JST時間なのにどこかでTimezone情報なくなって時刻表記だけになる→UTCとして解釈される→JST環境では更に+9時間されて本来より9時間多い値になるとか？
+		condition.Timestamp.Add(-9 * time.Hour).Unix(),
 		condition.IsSitting,
 		condition.IsDirty,
 		condition.IsOverweight,
 		condition.IsBroken,
 		condition.Message,
 		condition.CreatedAt,
+		condition.Isu.JIAIsuUUID,
+		isuId,
+		condition.ConditionLevel(),
 	}
 	switch condition.ConditionLevel() {
 	case ConditionLevelInfo:
