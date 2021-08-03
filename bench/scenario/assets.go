7b1d963698f9d3631405278c51a1168c95675eec
ryoha000 <the7uya@gmail.com>
2021-08-03 21:59:42 +0900

    fix(bench) assets.goの更新

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -4,18 +4,18 @@ package scenario
 var (
 	resourcesHash = map[string]string{ 
 		"/favicon.d0f5f504.svg": "3697a1b6c6d164ec4a5e4db6f7789f05",
-		"/index.994e7d97.css": "69d7a3d9d51474ea10065c11f6d6f08a",
-		"/index.fab163f8.js": "7d2c49ec3ece26e88229adeab55d160e",
+		"/index.791988ac.js": "5fef788383c928b4e749e784b480d679",
+		"/index.e37999c4.css": "4701c0da3d9a19730a9fd0d1deca021c",
 		"/logo_orange.974bf3e6.svg": "10e2dfa0ff27ec4325b781973c646737",
 		"/logo_white.98008342.svg": "f81e35a6821af44ea5b3a77346bcf6ad",
-		"/vendor.336960d7.js": "750beb8831a9353f78c7b06bf8dc766c",
+		"/vendor.7e9a2e2c.js": "e3b95fef34ccf4a597ff49c1f886e856",
 	}
 	resourcesMap = map[string]string{ 
 		"/favicon.svg": "/favicon.d0f5f504.svg",
-		"/index.css": "/index.994e7d97.css",
-		"/index.js": "/index.fab163f8.js",
+		"/index.css": "/index.e37999c4.css",
+		"/index.js": "/index.791988ac.js",
 		"/logo_orange.svg": "/logo_orange.974bf3e6.svg",
 		"/logo_white.svg": "/logo_white.98008342.svg",
-		"/vendor.js": "/vendor.336960d7.js",
+		"/vendor.js": "/vendor.7e9a2e2c.js",
 	}
 )
