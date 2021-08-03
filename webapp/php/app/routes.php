57eb7bef13ebd3837d74900bfc8c245897fc7927
Okada Shohei <okashoicircus0@gmail.com>
2021-08-04 02:03:45 +0900

    feat(backend-php): assets/ 以下のファイルを返す処理を実装

--- webapp/php/app/routes.php
+++ webapp/php/app/routes.php
@@ -18,10 +18,32 @@ return function (App $app) {
     $app->get('/isu/{jia_isu_uuid}', 'getIndex');
     $app->get('/register', 'getIndex');
     $app->get('/login', 'getIndex');
+
+    $app->get('/assets/{filename}', 'getAssets');
 };
 
 function getIndex(Request $request, Response $response): Response {
     $response->getBody()->write(file_get_contents(FRONTEND_CONTENTS_PATH . '/index.html'));
 
     return $response;
+}
+
+function getAssets(Request $request, Response $response, array $args): Response {
+    $filePath = FRONTEND_CONTENTS_PATH . '/assets/' . $args['filename'];
+
+    if (!file_exists($filePath)) {
+        return $response->withStatus(404, 'File Not Found');
+    }
+
+    $mimeType = match (pathinfo($filePath, PATHINFO_EXTENSION)) {
+        'js' => 'text/javascript',
+        'css' => 'text/css',
+        'svg' => 'image/svg+xml',
+        default => 'text/html',
+    };
+
+    $newResponse = $response->withHeader('Content-Type', $mimeType . '; charset=UTF-8');
+    $newResponse->getBody()->write(file_get_contents($filePath));
+
+    return $newResponse;
 }
