069c8eb90731f67e9667b58d6c6886109486efbd
ryoha000 <the7uya@gmail.com>
2021-08-03 23:03:37 +0900

    feat(frontend) GET /api/isu でconditionがもらえるのでそれを表示

--- webapp/frontend/src/components/UI/IsuList.tsx
+++ webapp/frontend/src/components/UI/IsuList.tsx
@@ -1,15 +1,21 @@
-import { Isu as IIsu } from '../../lib/apis'
+import { GetIsuListResponse } from '../../lib/apis'
+import { getConditionTime } from '../../lib/date'
 import Isu from './Isu'
+import Tip from './Tip'
 
 interface Props {
-  isus: IIsu[]
+  isus: GetIsuListResponse[]
 }
 
 const IsuList = ({ isus }: Props) => {
   return (
     <div className="grid gap-8 grid-cols-isus w-full">
       {isus.map(isu => (
-        <Isu isu={isu} key={isu.jia_isu_uuid} />
+        <div key={isu.jia_isu_uuid} className="flex flex-col items-center">
+          <Isu isu={isu} />
+          <div>{getConditionTime(isu.latest_isu_condition)}</div>
+          <Tip variant={isu.latest_isu_condition.condition_level} />
+        </div>
       ))}
     </div>
   )
