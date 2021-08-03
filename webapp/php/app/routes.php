20a6d245f473e1b7a1ad1bfbff5ba25630d386db
Okada Shohei <okashoicircus0@gmail.com>
2021-08-04 02:03:10 +0900

    feat(backend-php): index.html を返す処理を実装

--- webapp/php/app/routes.php
+++ webapp/php/app/routes.php
@@ -5,14 +5,23 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
+const FRONTEND_CONTENTS_PATH = __DIR__ . '/../../public';
+
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
         return $response;
     });
 
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
-    });
+    $app->get('/', 'getIndex');
+    $app->get('/condition', 'getIndex');
+    $app->get('/isu/{jia_isu_uuid}', 'getIndex');
+    $app->get('/register', 'getIndex');
+    $app->get('/login', 'getIndex');
 };
+
+function getIndex(Request $request, Response $response): Response {
+    $response->getBody()->write(file_get_contents(FRONTEND_CONTENTS_PATH . '/index.html'));
+
+    return $response;
+}
