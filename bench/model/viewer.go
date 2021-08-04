4b4d5f267328b3c352829f4ef60611fcd2463508
ShotaKitazawa <skitazawa1121@gmail.com>
2021-08-04 23:52:50 +0900

    fix(bench) 前回の GET trend における condition の結果を scenario ではなく viewer 毎に持つようにした

--- bench/model/viewer.go
+++ bench/model/viewer.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"sync"
+
 	"github.com/isucon/isucandar/agent"
 )
 
@@ -9,12 +11,35 @@ type Viewer struct {
 	ErrorCount         uint
 	ViewedUpdatedCount uint
 	Agent              *agent.Agent
+
+	// GET trend にて既に確認したconditionを格納するのに利用
+	verifiedConditionsInTrend      map[isuIDAndConditionTimestamp]struct{}
+	verifiedConditionsInTrendMutex sync.RWMutex
+}
+
+type isuIDAndConditionTimestamp struct {
+	IsuID              int
+	ConditionTimestamp int64
 }
 
 func NewViewer(agent *agent.Agent) Viewer {
 	return Viewer{
-		ErrorCount:         0,
-		ViewedUpdatedCount: 0,
-		Agent:              agent,
+		ErrorCount:                0,
+		ViewedUpdatedCount:        0,
+		Agent:                     agent,
+		verifiedConditionsInTrend: make(map[isuIDAndConditionTimestamp]struct{}, 8192),
 	}
+}
+
+func (v *Viewer) SetVerifiedCondition(id int, timestamp int64) {
+	v.verifiedConditionsInTrendMutex.Lock()
+	defer v.verifiedConditionsInTrendMutex.Unlock()
+	v.verifiedConditionsInTrend[isuIDAndConditionTimestamp{id, timestamp}] = struct{}{}
+}
+
+func (v *Viewer) ConditionAlreadyVerified(id int, timestamp int64) bool {
+	v.verifiedConditionsInTrendMutex.RLock()
+	defer v.verifiedConditionsInTrendMutex.RUnlock()
+	_, exist := v.verifiedConditionsInTrend[isuIDAndConditionTimestamp{id, timestamp}]
+	return exist
 }
