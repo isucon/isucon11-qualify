143a853fe2e70c79c4cc56c31e0d76470acbe552
ShotaKitazawa <skitazawa1121@gmail.com>
2021-08-05 01:14:51 +0900

    fix(infra) state の名前を更新

--- provisioning/terraform-practice/backend.tf
+++ provisioning/terraform-practice/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket               = "isucon11-misc"
     workspace_key_prefix = "terraform"
-    key                  = "terraform/qualify-20210710.tfstate"
+    key                  = "terraform/qualify-20210805.tfstate"
     region               = "ap-northeast-1"
     dynamodb_table       = "isucon11-terraform-locks"
   }
