aa95fa5bc00710b5f06b8e8f2121329785058072
Nagarei <Nagarei@users.noreply.github.com>
2021-08-04 18:50:46 +0900

    fix(bench): 更新されているかどうか確認

--- bench/scenario/load.go
+++ bench/scenario/load.go
@@ -143,13 +143,15 @@ func (s *Scenario) loadNormalUser(ctx context.Context, step *isucandar.Benchmark
 
 		//GET /
 		dataExistTimestamp := GetConditionDataExistTimestamp(s, user)
+		var newConditionUUIDs []string
 		_, errs := browserGetHomeAction(ctx, user.Agent, dataExistTimestamp, true,
 			func(res *http.Response, isuList []*service.Isu) []error {
 				// poster で送ったものの同期
 				//user.GetConditionFromChan(ctx)
 				expected := user.IsuListOrderByCreatedAt
 
-				_, errs := s.verifyIsuList(res, expected, isuList)
+				var errs []error
+				newConditionUUIDs, errs = s.verifyIsuList(res, expected, isuList)
 				return errs
 			},
 		)
@@ -158,6 +160,19 @@ func (s *Scenario) loadNormalUser(ctx context.Context, step *isucandar.Benchmark
 		}
 		if len(errs) > 0 {
 			continue
+		}
+		//更新されているかどうか確認
+		{
+			found := false
+			for _, updated := range newConditionUUIDs {
+				if updated == targetIsu.JIAIsuUUID {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue //更新されていない
+			}
 		}
 
 		//GET /isu/{jia_isu_uuid}
