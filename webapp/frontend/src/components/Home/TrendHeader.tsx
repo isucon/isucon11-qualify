e5b27b8fb64acb3af858bd4235e7ced0b346089b
ryoha000 <the7uya@gmail.com>
2021-08-04 21:27:52 +0900

    fix(frontend) 文字の修正

--- webapp/frontend/src/components/Home/TrendHeader.tsx
+++ webapp/frontend/src/components/Home/TrendHeader.tsx
@@ -4,13 +4,12 @@ const TrendHeadeer = () => {
   return (
     <div className="grid grid-cols-trend p-2">
       <div className="flex justify-center">
-        <div>性格</div>
+        <div>せいかく</div>
       </div>
       <div className="flex gap-4 items-center justify-center">
-        <div>ISUの数</div>
-        <Tip variant="info" />
-        <Tip variant="warning" />
-        <Tip variant="critical" />
+        <Tip variant="info" label="バッチリ" />
+        <Tip variant="warning" label="ぼちぼち" />
+        <Tip variant="critical" label="こわれた" />
       </div>
     </div>
   )
