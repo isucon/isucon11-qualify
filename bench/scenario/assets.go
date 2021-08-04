5570337150fae6a415cddca2ffbcd5b89714f8f0
ryoha000 <the7uya@gmail.com>
2021-08-04 19:29:47 +0900

    fix(bench) fix assets

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -4,18 +4,18 @@ package scenario
 var (
 	resourcesHash = map[string]string{ 
 		"/favicon.d0f5f504.svg": "3697a1b6c6d164ec4a5e4db6f7789f05",
-		"/index.0d3741e5.css": "754e54ce0a55e20c8380c24ee4cc9665",
-		"/index.7ef4f42f.js": "b4bb9feee507dbb4193de6a779bec00f",
+		"/index.6faed18c.js": "6562f4e3a759f66afdc33833aa4d7511",
+		"/index.d6291377.css": "578beb8fd35dfed8e4402f3d05d57713",
 		"/logo_orange.974bf3e6.svg": "10e2dfa0ff27ec4325b781973c646737",
 		"/logo_white.98008342.svg": "f81e35a6821af44ea5b3a77346bcf6ad",
-		"/vendor.7e9a2e2c.js": "e3b95fef34ccf4a597ff49c1f886e856",
+		"/vendor.d4d4cae8.js": "46fa84f37d804e126912ed51fa926f06",
 	}
 	resourcesMap = map[string]string{ 
 		"/favicon.svg": "/favicon.d0f5f504.svg",
-		"/index.css": "/index.0d3741e5.css",
-		"/index.js": "/index.7ef4f42f.js",
+		"/index.css": "/index.d6291377.css",
+		"/index.js": "/index.6faed18c.js",
 		"/logo_orange.svg": "/logo_orange.974bf3e6.svg",
 		"/logo_white.svg": "/logo_white.98008342.svg",
-		"/vendor.js": "/vendor.7e9a2e2c.js",
+		"/vendor.js": "/vendor.d4d4cae8.js",
 	}
 )
