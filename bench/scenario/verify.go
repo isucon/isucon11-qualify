9103479185b8a0a9dae5d1b4f40ef7343c80c906
ShotaKitazawa <skitazawa1121@gmail.com>
2021-08-06 18:05:07 +0900

    fix(bench) #803 コメントに伴う修正

--- bench/scenario/verify.go
+++ bench/scenario/verify.go
@@ -160,7 +160,7 @@ func verifyIsuConditions(res *http.Response,
 		case 'c':
 			filter |= model.ConditionLevelCritical
 		default:
-			panic(fmt.Errorf("verifyIsuCondtions: リクエストのクエリパラメータ condition_level が不正な値です: %v", level))
+			logger.AdminLogger.Panicf("verifyIsuCondtions: リクエストのクエリパラメータ condition_level が不正な値です: %v", level)
 		}
 	}
 
@@ -272,7 +272,7 @@ func verifyPrepareIsuConditions(res *http.Response,
 		case 'c':
 			filter |= model.ConditionLevelCritical
 		default:
-			panic(fmt.Errorf("verifyPrepareIsuCondtions: リクエストのクエリパラメータ condition_level が不正な値です: %v", level))
+			logger.AdminLogger.Panicf("verifyPrepareIsuCondtions: リクエストのクエリパラメータ condition_level が不正な値です: %v", level)
 		}
 	}
 
