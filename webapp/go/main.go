bcd75e15f4e2c8f861f40c6fc5fbe269fd574a5e
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2021-08-06 19:57:57 +0900

    fix(backend):getUserIDFromSessionはエラー時にステータスコードを返す

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -277,15 +277,14 @@ func getSession(r *http.Request) (*sessions.Session, error) {
 	return session, nil
 }
 
-func getUserIDFromSession(c echo.Context) (string, error) {
+func getUserIDFromSession(c echo.Context) (string, int, error) {
 	session, err := getSession(c.Request())
 	if err != nil {
-		c.Logger().Errorf("failed to get session: %v", err)
-		return "", c.NoContent(http.StatusInternalServerError)
+		return "", http.StatusInternalServerError, fmt.Errorf("failed to get session: %v", err)
 	}
 	_jiaUserID, ok := session.Values["jia_user_id"]
 	if !ok {
-		return "", c.String(http.StatusUnauthorized, "you are not signed in")
+		return "", http.StatusUnauthorized, fmt.Errorf("no session")
 	}
 
 	jiaUserID := _jiaUserID.(string)
@@ -294,15 +293,14 @@ func getUserIDFromSession(c echo.Context) (string, error) {
 	err = db.Get(&count, "SELECT COUNT(*) FROM `user` WHERE `jia_user_id` = ?",
 		jiaUserID)
 	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return "", c.NoContent(http.StatusInternalServerError)
+		return "", http.StatusInternalServerError, fmt.Errorf("db error: %v", err)
 	}
 
 	if count == 0 {
-		return "", c.String(http.StatusUnauthorized, "you are not signed in")
+		return "", http.StatusUnauthorized, fmt.Errorf("not found: user")
 	}
 
-	return jiaUserID, nil
+	return jiaUserID, 0, nil
 }
 
 func getJIAServiceURL(tx *sqlx.Tx) string {
@@ -415,9 +413,14 @@ func postAuthentication(c echo.Context) error {
 
 //  POST /api/signout
 func postSignout(c echo.Context) error {
-	_, err := getUserIDFromSession(c)
+	_, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		} else {
+			c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
 	}
 
 	session, err := getSession(c.Request())
@@ -437,9 +440,14 @@ func postSignout(c echo.Context) error {
 }
 
 func getMe(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	res := GetMeResponse{JIAUserID: jiaUserID}
@@ -449,9 +457,14 @@ func getMe(c echo.Context) error {
 //  GET /api/isu
 // 自分のISUの一覧を取得
 func getIsuList(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	tx, err := db.Beginx()
@@ -526,9 +539,14 @@ func getIsuList(c echo.Context) error {
 //  POST /api/isu
 // 自分のISUの登録
 func postIsu(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	useDefaultImage := false
@@ -655,9 +673,14 @@ func postIsu(c echo.Context) error {
 //  GET /api/isu/{jia_isu_uuid}
 // 椅子の情報を取得する
 func getIsu(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
@@ -684,9 +707,14 @@ func getIsu(c echo.Context) error {
 //       https://tech.jxpress.net/entry/2018/08/23/104123
 // MEMO: DB 内の image は longblob
 func getIsuIcon(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
@@ -713,9 +741,14 @@ func getIsuIcon(c echo.Context) error {
 // 日毎時間単位グラフ
 // conditionを何件か集めて、ISUにとっての快適度数みたいな値を算出する
 func getIsuGraph(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
@@ -924,10 +957,16 @@ func calculateGraphDataPoint(isuConditions []IsuCondition) (GraphDataPoint, erro
 //  GET /api/condition/{jia_isu_uuid}?
 // 自分の所持椅子のうち、指定した椅子の通知を取得する
 func getIsuConditions(c echo.Context) error {
-	jiaUserID, err := getUserIDFromSession(c)
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
 	if err != nil {
-		return err
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Errorf("failed to getUserIDFromSession: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
+
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 	if jiaIsuUUID == "" {
 		return c.String(http.StatusBadRequest, "missing: jia_isu_uuid")
