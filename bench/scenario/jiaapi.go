8a16034b5f5fc03ad3a9a766f037642f05a3d5bc
ShotaKitazawa <skitazawa1121@gmail.com>
2021-08-10 15:59:34 +0900

    feat(infra) HTTPS化に伴う更新

--- bench/scenario/jiaapi.go
+++ bench/scenario/jiaapi.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 	"sync"
 	"time"
 
@@ -98,6 +99,7 @@ func (s *Scenario) postActivate(c echo.Context) error {
 	//poster Goroutineの起動
 	var isu *model.Isu
 	var scenarioChan *model.StreamsForPoster
+	var fqdn string
 	posterContext := posterRootContext
 	err = func() error {
 		var ok bool
@@ -108,6 +110,20 @@ func (s *Scenario) postActivate(c echo.Context) error {
 		if !ok {
 			return echo.NewHTTPError(http.StatusNotFound)
 		}
+		// FQDN が競技者 VM のものでない場合 400 を返す
+		fqdn = targetBaseURL.Hostname()
+		port := targetBaseURL.Port()
+		ipAddr, ok := s.GetIPAddrFromFqdn(fqdn)
+		if !ok {
+			return echo.NewHTTPError(http.StatusBadRequest)
+		}
+		// URL の文字列を IP アドレスに変換
+		if port != "" {
+			targetBaseURL.Host = strings.Join([]string{ipAddr, port}, ":")
+		} else {
+			targetBaseURL.Host = ipAddr
+		}
+		// リクエストされた JIA_ISU_UUID が事前に scenario.NewIsu にて作成された isu と紐付かない場合 403 を返す
 		isu, ok = isuFromUUID[state.IsuUUID]
 		if !ok {
 			//scenarioChanでチェックしているのでここには来ないはず
@@ -126,7 +142,7 @@ func (s *Scenario) postActivate(c echo.Context) error {
 		s.loadWaitGroup.Add(1)
 		go func() {
 			defer s.loadWaitGroup.Done()
-			s.keepPosting(posterContext, targetBaseURL, isu, scenarioChan)
+			s.keepPosting(posterContext, targetBaseURL, fqdn, isu, scenarioChan)
 		}()
 		return nil
 	}()
