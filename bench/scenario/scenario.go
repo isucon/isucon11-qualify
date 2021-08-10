f4adbbe5dad9175e9f4365e91e2a8c382db712d6
ryoha000 <the7uya@gmail.com>
2021-08-10 17:33:31 +0900

    fix(bench) trendの返り値を捨てるときにunmarshalしないように

--- bench/scenario/scenario.go
+++ bench/scenario/scenario.go
@@ -138,7 +138,7 @@ func (s *Scenario) NewUser(ctx context.Context, step *isucandar.BenchmarkStep, a
 	//backendにpostする
 	go func() {
 		// 登録済みユーザーは trend に興味がないからリクエストを待たない
-		browserGetLandingPageAction(ctx, a)
+		browserGetLandingPageIgnoreAction(ctx, a)
 	}()
 	//TODO: 確率で失敗してリトライする
 	_, errs := authAction(ctx, a, user.UserID)
