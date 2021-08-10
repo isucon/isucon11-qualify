f6d7b9764ddcd0ff7c430a383652e868e2bcefb4
ryoha000 <the7uya@gmail.com>
2021-08-11 08:00:04 +0900

    feat(frontend) ログインボタンをヘッダーに追加

--- webapp/frontend/src/components/PageHeader/Controls.tsx
+++ webapp/frontend/src/components/PageHeader/Controls.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import ControlItem from './ControlItem'
 import UserControlModal from './UserControlModal'
 import ControlLinkItem from './ControlLinkItem'
-import { IoIosNotifications } from 'react-icons/io'
+import { BiLogInCircle } from 'react-icons/bi'
 import { TiPlus } from 'react-icons/ti'
 
 const Controls = () => {
@@ -15,7 +15,15 @@ const Controls = () => {
   }
   const me = useStateContext().me
   if (!me) {
-    return <></>
+    return (
+      <div className="flex items-center justify-between ml-auto">
+        <ControlLinkItem
+          to="/login"
+          label="ログイン"
+          icon={<BiLogInCircle />}
+        />
+      </div>
+    )
   }
 
   return (
