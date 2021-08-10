7b8c84a81b05b28db9ca3f62797f28a4bff934fa
hoto <mail@hoto.me>
2021-08-10 22:17:09 +0900

    [python] define endpoints

--- webapp/python/main.py
+++ webapp/python/main.py
@@ -18,11 +18,6 @@ mysql_connection_env = {
 cnxpool = QueuePool(lambda: mysql.connector.connect(**mysql_connection_env), pool_size=10)
 
 
-@app.route("/", methods=["GET"])
-def get_index():
-    return send_file("../public/index.html")
-
-
 @app.route("/initialize", methods=["POST"])
 def post_initialize():
     if "jia_service_url" not in request.json:
@@ -42,6 +37,86 @@ def post_initialize():
         cnx.close()
 
     return {"Language": "python"}
+
+
+@app.route("/api/auth", methods=["POST"])
+def post_auth():
+    raise NotImplementedError
+
+
+@app.route("/api/signout", methods=["POST"])
+def post_signout():
+    raise NotImplementedError
+
+
+@app.route("/api/user/me", methods=["GET"])
+def get_me():
+    raise NotImplementedError
+
+
+@app.route("/api/isu", methods=["GET"])
+def get_isu_list():
+    raise NotImplementedError
+
+
+@app.route("/api/isu", methods=["POST"])
+def post_isu():
+    raise NotImplementedError
+
+
+@app.route("/api/isu/:jia_isu_uuid", methods=["GET"])
+def get_isu_id():
+    raise NotImplementedError
+
+
+@app.route("/api/isu/:jia_isu_uuid/icon", methods=["GET"])
+def get_isu_icon():
+    raise NotImplementedError
+
+
+@app.route("/api/isu/:jia_isu_uuid/graph", methods=["GET"])
+def get_isu_graph():
+    raise NotImplementedError
+
+
+@app.route("/api/condition/:jia_isu_uuid", methods=["GET"])
+def get_isu_confitions():
+    raise NotImplementedError
+
+
+@app.route("/api/trend", methods=["GET"])
+def get_trend():
+    raise NotImplementedError
+
+
+@app.route("/api/condition/:jia_isu_uuid", methods=["POST"])
+def post_isu_condition():
+    raise NotImplementedError
+
+
+@app.route("/", methods=["GET"])
+def get_index():
+    return send_file("../public/index.html")
+
+
+@app.route("/condition", methods=["GET"])
+def get_condition():
+    return send_file("../public/index.html")
+
+
+@app.route("/isu/:jia_isu_uuid", methods=["GET"])
+def get_isu():
+    return send_file("../public/index.html")
+
+
+@app.route("/register", methods=["GET"])
+def get_register():
+    return send_file("../public/index.html")
+
+
+@app.route("/login", methods=["GET"])
+def get_login():
+    return send_file("../public/index.html")
 
 
 if __name__ == "__main__":
