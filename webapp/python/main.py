03763b9a3197b4cf599cc79884ed845930e3f2a4
hoto <mail@hoto.me>
2021-08-11 11:10:02 +0900

    [python] /api/auth

--- webapp/python/main.py
+++ webapp/python/main.py
@@ -1,11 +1,13 @@
 from os import getenv
 from subprocess import call
-from flask import Flask, request, send_file
-from werkzeug.exceptions import BadRequest
+from flask import Flask, request, session, send_file
+from werkzeug.exceptions import BadRequest, Unauthorized
 import mysql.connector
 from sqlalchemy.pool import QueuePool
+import jwt
 
 app = Flask(__name__, static_folder="../public/assets", static_url_path="/assets")
+app.secret_key = getenv("SESSION_KEY", "isucondition")
 
 mysql_connection_env = {
     "host": getenv("MYSQL_HOST", "127.0.0.1"),
@@ -16,6 +18,40 @@ mysql_connection_env = {
 }
 
 cnxpool = QueuePool(lambda: mysql.connector.connect(**mysql_connection_env), pool_size=10)
+
+
+def select_all(query, *args, dictionary=True):
+    cnx = cnxpool.connect()
+    try:
+        cur = cnx.cursor(dictionary=dictionary)
+        cur.execute(query, *args)
+        return cur.fetchall()
+    finally:
+        cnx.close()
+
+
+def select_row(*args, **kwargs):
+    rows = select_all(*args, **kwargs)
+    return rows[0] if len(rows) > 0 else None
+
+
+with open("../ec256-public.pem", "rb") as f:
+    jwt_public_key = f.read()
+
+
+def get_user_id_from_session():
+    jia_user_id = session.get("jia_user_id")
+
+    if jia_user_id is None:
+        raise Unauthorized("no session")
+
+    query = "SELECT COUNT(*) FROM `user` WHERE `jia_user_id` = %s"
+    (count,) = select_row(query, (jia_user_id,), dictionary=False)
+
+    if count == 0:
+        raise Unauthorized("not found: user")
+
+    return jia_user_id
 
 
 @app.route("/initialize", methods=["POST"])
@@ -31,8 +67,6 @@ def post_initialize():
         query = "INSERT INTO `isu_association_config` (`name`, `url`) VALUES (%s, %s) ON DUPLICATE KEY UPDATE `url` = VALUES(`url`)"
         cur.execute(query, ("jia_service_url", request.json["jia_service_url"]))
         cnx.commit()
-    except Exception:
-        raise  # TODO raise 500
     finally:
         cnx.close()
 
@@ -41,17 +75,36 @@ def post_initialize():
 
 @app.route("/api/auth", methods=["POST"])
 def post_auth():
-    raise NotImplementedError
+    req_jwt = request.headers["Authorization"][len("Bearer ") :]
+    req_jwt_header = jwt.get_unverified_header(req_jwt)
+    req_jwt_payload = jwt.decode(req_jwt, jwt_public_key, algorithms=[req_jwt_header["alg"]])
+    jia_user_id = req_jwt_payload["jia_user_id"]
+
+    cnx = cnxpool.connect()
+    try:
+        cur = cnx.cursor()
+        query = "INSERT IGNORE INTO user (`jia_user_id`) VALUES (%s)"
+        cur.execute(query, (jia_user_id,))
+        cnx.commit()
+    finally:
+        cnx.close()
+
+    session["jia_user_id"] = jia_user_id
+
+    return ""
 
 
 @app.route("/api/signout", methods=["POST"])
 def post_signout():
-    raise NotImplementedError
+    get_user_id_from_session()
+    session.clear()
+    return ""
 
 
 @app.route("/api/user/me", methods=["GET"])
 def get_me():
-    raise NotImplementedError
+    jia_user_id = get_user_id_from_session()
+    return {"jia_user_id": jia_user_id}
 
 
 @app.route("/api/isu", methods=["GET"])
