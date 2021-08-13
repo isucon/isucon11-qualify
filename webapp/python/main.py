e186a3de52ca94301f107df3165e3fe66cb11c6b
hoto <mail@hoto.me>
2021-08-14 05:10:37 +0900

    [python] GET /api/trend

--- webapp/python/main.py
+++ webapp/python/main.py
@@ -97,6 +97,20 @@ class GetIsuConditionResponse:
     message: str
 
 
+@dataclass
+class TrendCondition:
+    isu_id: int
+    timestamp: int
+
+
+@dataclass
+class TrendResponse:
+    character: str
+    info: TrendCondition
+    warning: TrendCondition
+    critical: TrendCondition
+
+
 class CustomJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
@@ -619,7 +633,49 @@ def get_isu_condition_from_db(
 @app.route("/api/trend", methods=["GET"])
 def get_trend():
     """ISUの性格毎の最新のコンディション情報"""
-    raise NotImplementedError
+    query = "SELECT `character` FROM `isu` GROUP BY `character`"
+    character_list = [row["character"] for row in select_all(query)]
+
+    res = []
+
+    for character in character_list:
+        query = "SELECT * FROM `isu` WHERE `character` = %s"
+        isu_list = [Isu(**row) for row in select_all(query, (character,))]
+
+        character_info_isu_conditions = []
+        character_warning_isu_conditions = []
+        character_critical_isu_conditions = []
+        for isu in isu_list:
+            query = "SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = %s ORDER BY timestamp DESC"
+            conditions = [IsuCondition(**row) for row in select_all(query, (isu.jia_isu_uuid,))]
+
+            if len(conditions) > 0:
+                isu_last_condition = conditions[0]
+                condition_level = calculate_condition_level(isu_last_condition.condition)
+
+                trend_condition = TrendCondition(isu_id=isu.id, timestamp=isu_last_condition.timestamp.timestamp())
+
+                if condition_level == "info":
+                    character_info_isu_conditions.append(trend_condition)
+                elif condition_level == "warning":
+                    character_warning_isu_conditions.append(trend_condition)
+                elif condition_level == "critical":
+                    character_critical_isu_conditions.append(trend_condition)
+
+        character_info_isu_conditions.sort(key=lambda c: c.timestamp)
+        character_warning_isu_conditions.sort(key=lambda c: c.timestamp)
+        character_critical_isu_conditions.sort(key=lambda c: c.timestamp)
+
+        res.append(
+            TrendResponse(
+                character=character,
+                info=character_info_isu_conditions,
+                warning=character_warning_isu_conditions,
+                critical=character_critical_isu_conditions,
+            )
+        )
+
+    return jsonify(res)
 
 
 @app.route("/api/condition/<jia_isu_uuid>", methods=["POST"])
