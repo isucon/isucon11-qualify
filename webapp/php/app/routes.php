9e6efeb10ba00da3589a15313e6c4ab8c611d210
Okada Shohei <okashoicircus0@gmail.com>
2021-08-14 16:15:51 +0900

    chore(backend-php): DI コンテナが利用できるように、ハンドラを関数ではなくクラスのメソッドとして定義するように変更

--- webapp/php/app/routes.php
+++ webapp/php/app/routes.php
@@ -7,47 +7,50 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
-const FRONTEND_CONTENTS_PATH = __DIR__ . '/../../public';
-
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
         return $response;
     });
 
-    $app->get('/', 'getIndex');
-    $app->get('/condition', 'getIndex');
-    $app->get('/isu/{jia_isu_uuid}', 'getIndex');
-    $app->get('/register', 'getIndex');
-    $app->get('/login', 'getIndex');
+    $app->get('/', Handler::class . ':getIndex');
+    $app->get('/condition', Handler::class . ':getIndex');
+    $app->get('/isu/{jia_isu_uuid}', Handler::class . ':getIndex');
+    $app->get('/register', Handler::class . ':getIndex');
+    $app->get('/login', Handler::class . ':getIndex');
 
-    $app->get('/assets/{filename}', 'getAssets');
+    $app->get('/assets/{filename}', Handler::class . ':getAssets');
 };
 
-function getIndex(Request $request, Response $response): Response
+final class Handler
 {
-    $response->getBody()->write(file_get_contents(FRONTEND_CONTENTS_PATH . '/index.html'));
+    private const FRONTEND_CONTENTS_PATH = __DIR__ . '/../../public';
 
-    return $response;
-}
+    public function getIndex(Request $request, Response $response): Response
+    {
+        $response->getBody()->write(file_get_contents(self::FRONTEND_CONTENTS_PATH . '/index.html'));
 
-function getAssets(Request $request, Response $response, array $args): Response
-{
-    $filePath = FRONTEND_CONTENTS_PATH . '/assets/' . $args['filename'];
-
-    if (!file_exists($filePath)) {
-        return $response->withStatus(404, 'File Not Found');
+        return $response;
     }
 
-    $mimeType = match (pathinfo($filePath, PATHINFO_EXTENSION)) {
-        'js' => 'text/javascript',
-        'css' => 'text/css',
-        'svg' => 'image/svg+xml',
-        default => 'text/html',
-    };
+    public function getAssets(Request $request, Response $response, array $args): Response
+    {
+        $filePath = self::FRONTEND_CONTENTS_PATH . '/assets/' . $args['filename'];
 
-    $newResponse = $response->withHeader('Content-Type', $mimeType . '; charset=UTF-8');
-    $newResponse->getBody()->write(file_get_contents($filePath));
+        if (!file_exists($filePath)) {
+            return $response->withStatus(404, 'File Not Found');
+        }
 
-    return $newResponse;
+        $mimeType = match (pathinfo($filePath, PATHINFO_EXTENSION)) {
+            'js' => 'text/javascript',
+            'css' => 'text/css',
+            'svg' => 'image/svg+xml',
+            default => 'text/html',
+        };
+
+        $newResponse = $response->withHeader('Content-Type', $mimeType . '; charset=UTF-8');
+        $newResponse->getBody()->write(file_get_contents($filePath));
+
+        return $newResponse;
+    }
 }
