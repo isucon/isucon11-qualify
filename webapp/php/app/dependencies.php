74aa0ae72c9e60ac00acaecf2b3125e19d0d16a2
Okada Shohei <okashoicircus0@gmail.com>
2021-08-14 16:04:38 +0900

    chore(backend-php): PDO のデフォルト fetch mode を PDO::FETCH_BOTH に変更

--- webapp/php/app/dependencies.php
+++ webapp/php/app/dependencies.php
@@ -39,7 +39,6 @@ return function (ContainerBuilder $containerBuilder) {
 
             $pdo = new PDO($dsn, $databaseSettings['user'], $databaseSettings['password']);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
             return $pdo;
         },
