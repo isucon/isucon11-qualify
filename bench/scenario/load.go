649e61567f431a5d44ac8fadd6233d0adc24f10d
ryoha000 <the7uya@gmail.com>
2021-08-16 00:18:54 +0900

    fix(bench) 負荷レベルの文言を削除

--- bench/scenario/load.go
+++ bench/scenario/load.go
@@ -138,11 +138,11 @@ func (s *Scenario) userAdder(ctx context.Context, step *isucandar.BenchmarkStep)
 		addStep := AddUserStep * atomic.LoadInt32(&userLoopCount)
 		addCount := atomic.LoadInt32(&viewUpdatedTrendCounter) / addStep
 		if addCount > 0 {
-			logger.ContestantLogger.Printf("現レベルの負荷へ応答ができているため、負荷レベルを%d上昇させます", addCount)
+			logger.ContestantLogger.Printf("現レベルの負荷へ応答ができているため、ユーザーを%d人追加します", addCount)
 			s.AddNormalUser(ctx, step, AddUserCount*int(addCount))
 			atomic.AddInt32(&viewUpdatedTrendCounter, -addStep*addCount)
 		} else {
-			logger.ContestantLogger.Println("負荷レベルは上昇しませんでした")
+			logger.ContestantLogger.Println("ユーザーは追加されませんでした")
 		}
 	}
 }
