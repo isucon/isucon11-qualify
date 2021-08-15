188b6efad552364853f6ddb7adb662e813fc8042
sugyan <sugi1982@gmail.com>
2021-08-15 14:03:00 +0900

    fix(nodejs): Fix dropProbability value

--- webapp/nodejs/src/app.ts
+++ webapp/nodejs/src/app.ts
@@ -1100,7 +1100,7 @@ app.post(
     res
   ) => {
     // TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-    const dropProbability = 0.5;
+    const dropProbability = 0.9;
     if (Math.random() < dropProbability) {
       console.warn("drop post isu condition request");
       return res.status(503).send();
