def6289582b59b6d139077c9b3f9c6267a566347
ryoha000 <the7uya@gmail.com>
2021-08-17 17:04:07 +0900

    fix(bench) update assets & logoの取得をするように

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -4,20 +4,28 @@ package scenario
 var (
 	resourcesHash = map[string]string{ 
 		"/assets/favicon.d0f5f504.svg": "63f35b9a",
+		"/assets/favicon_black.svg": "fe0f6eb7",
+		"/assets/favicon_orange.svg": "63f35b9a",
+		"/assets/favicon_white.svg": "dcfbe5a7",
+		"/assets/index.34f56915.js": "482220ce",
 		"/assets/index.48c3a998.css": "b569ef88",
-		"/assets/index.98041f41.js": "1b33be1d",
-		"/assets/logo_orange.974bf3e6.svg": "6c250f83",
-		"/assets/logo_white.98008342.svg": "61d68fa0",
-		"/assets/vendor.46946e6f.js": "6ab3edeb",
-		"/index.html": "68747f3b",
+		"/assets/logo_black.svg": "91648521",
+		"/assets/logo_orange.svg": "6c250f83",
+		"/assets/logo_white.svg": "61d68fa0",
+		"/assets/vendor.7c82aa44.js": "6ab3edeb",
+		"/index.html": "6beb71ed",
 	}
 	resourcesMap = map[string]string{ 
 		"/assets/favicon.svg": "/assets/favicon.d0f5f504.svg",
+		"/assets/favicon_black.svg": "/assets/favicon_black.svg",
+		"/assets/favicon_orange.svg": "/assets/favicon_orange.svg",
+		"/assets/favicon_white.svg": "/assets/favicon_white.svg",
 		"/assets/index.css": "/assets/index.48c3a998.css",
-		"/assets/index.js": "/assets/index.98041f41.js",
-		"/assets/logo_orange.svg": "/assets/logo_orange.974bf3e6.svg",
-		"/assets/logo_white.svg": "/assets/logo_white.98008342.svg",
-		"/assets/vendor.js": "/assets/vendor.46946e6f.js",
+		"/assets/index.js": "/assets/index.34f56915.js",
+		"/assets/logo_black.svg": "/assets/logo_black.svg",
+		"/assets/logo_orange.svg": "/assets/logo_orange.svg",
+		"/assets/logo_white.svg": "/assets/logo_white.svg",
+		"/assets/vendor.js": "/assets/vendor.7c82aa44.js",
 		"/index.html": "/index.html",
 	}
 )
