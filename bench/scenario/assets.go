af6e18e6394af3d2c6c6fa4ed6443dbc132fd66e
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2021-08-17 21:09:20 +0900

    feat(frontend): update assets

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -7,13 +7,13 @@ var (
 		"/assets/favicon_black.svg": "fe0f6eb7",
 		"/assets/favicon_orange.svg": "63f35b9a",
 		"/assets/favicon_white.svg": "dcfbe5a7",
-		"/assets/index.1a89588e.js": "567603e6",
+		"/assets/index.9dd50c32.js": "67d13923",
 		"/assets/index.b936e317.css": "d7733f97",
 		"/assets/logo_black.svg": "91648521",
 		"/assets/logo_orange.svg": "6c250f83",
 		"/assets/logo_white.svg": "61d68fa0",
-		"/assets/vendor.8c55c931.js": "5d3951e5",
-		"/index.html": "a997ec59",
+		"/assets/vendor.42115f8b.js": "5d3951e5",
+		"/index.html": "9d30317d",
 	}
 	resourcesMap = map[string]string{ 
 		"/assets/favicon.svg": "/assets/favicon.d0f5f504.svg",
@@ -21,11 +21,11 @@ var (
 		"/assets/favicon_orange.svg": "/assets/favicon_orange.svg",
 		"/assets/favicon_white.svg": "/assets/favicon_white.svg",
 		"/assets/index.css": "/assets/index.b936e317.css",
-		"/assets/index.js": "/assets/index.1a89588e.js",
+		"/assets/index.js": "/assets/index.9dd50c32.js",
 		"/assets/logo_black.svg": "/assets/logo_black.svg",
 		"/assets/logo_orange.svg": "/assets/logo_orange.svg",
 		"/assets/logo_white.svg": "/assets/logo_white.svg",
-		"/assets/vendor.js": "/assets/vendor.8c55c931.js",
+		"/assets/vendor.js": "/assets/vendor.42115f8b.js",
 		"/index.html": "/index.html",
 	}
 )
