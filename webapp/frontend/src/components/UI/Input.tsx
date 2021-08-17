16c54ca0aaf9aff63a5767a59ad3e3377d096f06
oribe(oribe1115) <ayaka.k1n05h17a@gmail.com>
2021-08-17 17:42:08 +0900

    fix(fronetnd): Input要素の背景色を修正

--- webapp/frontend/src/components/UI/Input.tsx
+++ webapp/frontend/src/components/UI/Input.tsx
@@ -26,7 +26,7 @@ const Input = ({
       <input
         type="text"
         {...inputProps}
-        className="border-primary focus:border-primary px-2 py-1 h-8 border-solid rounded focus:outline-none shadow-none"
+        className="border-primary focus:border-primary bg-secondary px-2 py-1 h-8 border-solid rounded focus:outline-none shadow-none"
         value={value}
         onChange={e => setValue(e.target.value)}
       ></input>
