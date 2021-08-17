b17fdf1fc195518354c1d1351d8fd943e2812f6f
ryoha000 <the7uya@gmail.com>
2021-08-18 06:26:23 +0900

    fix(bench) assetを更新

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -2,22 +2,30 @@
 package scenario
 
 var (
-	resourcesHash = map[string]string{
-		"/assets/favicon.d0f5f504.svg":     "3697a1b6c6d164ec4a5e4db6f7789f05",
-		"/assets/index.1d2d12fb.css":       "85280abbd68c5a78d76c26f09a5df2ed",
-		"/assets/index.e4f8e9a8.js":        "015f6cff1635bfb6462c4ebf11405507",
-		"/assets/logo_orange.974bf3e6.svg": "10e2dfa0ff27ec4325b781973c646737",
-		"/assets/logo_white.98008342.svg":  "f81e35a6821af44ea5b3a77346bcf6ad",
-		"/assets/vendor.4977aaea.js":       "c642358addcc92b00260fd5a8b346377",
-		"/index.html":                      "0f0de66fcb3f31d4ce627a8d5f0c3e08",
+	resourcesHash = map[string]string{ 
+		"/assets/favicon.d0f5f504.svg": "63f35b9a",
+		"/assets/favicon_black.svg": "fe0f6eb7",
+		"/assets/favicon_orange.svg": "63f35b9a",
+		"/assets/favicon_white.svg": "dcfbe5a7",
+		"/assets/index.3b45f520.js": "db4b7c04",
+		"/assets/index.b936e317.css": "d7733f97",
+		"/assets/logo_black.svg": "91648521",
+		"/assets/logo_orange.svg": "6c250f83",
+		"/assets/logo_white.svg": "61d68fa0",
+		"/assets/vendor.8c55c931.js": "5d3951e5",
+		"/index.html": "b5340a05",
 	}
-	resourcesMap = map[string]string{
-		"/assets/favicon.svg":     "/assets/favicon.d0f5f504.svg",
-		"/assets/index.css":       "/assets/index.1d2d12fb.css",
-		"/assets/index.js":        "/assets/index.e4f8e9a8.js",
-		"/assets/logo_orange.svg": "/assets/logo_orange.974bf3e6.svg",
-		"/assets/logo_white.svg":  "/assets/logo_white.98008342.svg",
-		"/assets/vendor.js":       "/assets/vendor.4977aaea.js",
-		"/index.html":             "/index.html",
+	resourcesMap = map[string]string{ 
+		"/assets/favicon.svg": "/assets/favicon.d0f5f504.svg",
+		"/assets/favicon_black.svg": "/assets/favicon_black.svg",
+		"/assets/favicon_orange.svg": "/assets/favicon_orange.svg",
+		"/assets/favicon_white.svg": "/assets/favicon_white.svg",
+		"/assets/index.css": "/assets/index.b936e317.css",
+		"/assets/index.js": "/assets/index.3b45f520.js",
+		"/assets/logo_black.svg": "/assets/logo_black.svg",
+		"/assets/logo_orange.svg": "/assets/logo_orange.svg",
+		"/assets/logo_white.svg": "/assets/logo_white.svg",
+		"/assets/vendor.js": "/assets/vendor.8c55c931.js",
+		"/index.html": "/index.html",
 	}
 )
