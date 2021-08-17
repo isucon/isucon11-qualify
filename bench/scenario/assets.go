87d6c0db196801f7da971ef8be1c0079c2b84dbd
ryoha000 <the7uya@gmail.com>
2021-08-17 15:02:32 +0900

    fix(frontend,bench) update assets

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -5,19 +5,19 @@ var (
 	resourcesHash = map[string]string{ 
 		"/assets/favicon.d0f5f504.svg": "63f35b9a",
 		"/assets/index.48c3a998.css": "b569ef88",
-		"/assets/index.aa370161.js": "8955a45a",
+		"/assets/index.51b6addf.js": "b095e6b2",
 		"/assets/logo_orange.974bf3e6.svg": "6c250f83",
 		"/assets/logo_white.98008342.svg": "61d68fa0",
-		"/assets/vendor.ac07a7a3.js": "a0d320b",
-		"/index.html": "88e79624",
+		"/assets/vendor.4a6cbeed.js": "a0d320b",
+		"/index.html": "3324e0e4",
 	}
 	resourcesMap = map[string]string{ 
 		"/assets/favicon.svg": "/assets/favicon.d0f5f504.svg",
 		"/assets/index.css": "/assets/index.48c3a998.css",
-		"/assets/index.js": "/assets/index.aa370161.js",
+		"/assets/index.js": "/assets/index.51b6addf.js",
 		"/assets/logo_orange.svg": "/assets/logo_orange.974bf3e6.svg",
 		"/assets/logo_white.svg": "/assets/logo_white.98008342.svg",
-		"/assets/vendor.js": "/assets/vendor.ac07a7a3.js",
+		"/assets/vendor.js": "/assets/vendor.4a6cbeed.js",
 		"/index.html": "/index.html",
 	}
 )
