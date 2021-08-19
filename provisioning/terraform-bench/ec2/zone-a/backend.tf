960b77aa67c42777e664bfc62637d55b860408ca
ShotaKitazawa <skitazawa1121@gmail.com>
2021-08-19 20:28:10 +0900

    fix(infra) terraform-bench: 無駄なファイルの削除

--- provisioning/terraform-bench/ec2/zone-a/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "isucon11-misc"
-    workspace_key_prefix = "terraform"
-    key                  = "terraform/qualify-bench-zone-a.tfstate"
-    region               = "ap-northeast-1"
-    dynamodb_table       = "isucon11-terraform-locks"
-  }
-}
--- provisioning/terraform-bench/ecs/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.38"
-    }
-  }
-  required_version = ">= 0.14"
-}
