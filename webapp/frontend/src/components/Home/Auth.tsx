9ddcf75b490c15f769026f6abeac438dd13866d3
ockie1729 <you_oki1@yahoo.co.jp>
2021-08-20 16:18:35 +0900

    fix(front) JIA認証サービスへのURLを修正

--- webapp/frontend/src/components/Home/Auth.tsx
+++ webapp/frontend/src/components/Home/Auth.tsx
@@ -3,7 +3,7 @@ import Card from '/@/components/UI/Card'
 
 const Auth = () => {
   const click = async () => {
-    location.href = `http://jia-auth.xi.isucon.dev?callback=${location.protocol}//${location.host}`
+    location.href = `https://jia-auth.xi.isucon.dev?callback=${location.protocol}//${location.host}`
   }
 
   return (
