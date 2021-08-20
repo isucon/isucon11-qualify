eb4378251f9fb4eadbc9df1c3c2796f10655e1b8
ShotaKitazawa <skitazawa1121@gmail.com>
2021-08-20 15:57:05 +0900

    fix(infra) JIA_SERVICE_URL 環境変数を supervisor 起動前に生成するようにした

--- provisioning/ansible/roles/bench.supervisor/files/var/lib/cloud/scripts/per-instance/generate-env_aws.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cat << _EOF_ >> /home/isucon/isuxportal-supervisor.env
-JIA_SERVICE_URL="http://$(curl -s --retry 5 --retry-connrefused --max-time 10 --connect-timeout 5 http://169.254.169.254/latest/meta-data/public-ipv4):5000"
-_EOF_
-chown isucon: /home/isucon/isuxportal-supervisor.env
