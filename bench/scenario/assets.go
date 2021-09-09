e304eb05708c495410c508e1ef339ca521cd5db3
ShotaKitazawa <skitazawa1121@gmail.com>
2021-09-09 21:36:49 +0900

    update assets in bench

--- bench/scenario/assets.go
+++ bench/scenario/assets.go
@@ -8,12 +8,14 @@ var (
 		"/assets/favicon_orange.svg": "63f35b9a",
 		"/assets/favicon_white.svg": "dcfbe5a7",
 		"/assets/index.144d8ca8.css": "d4c612eb",
+		"/assets/index.23dac98b.js": "403ecf3b",
 		"/assets/index.6fe5d059.js": "693d9f4f",
 		"/assets/logo_black.svg": "91648521",
 		"/assets/logo_orange.svg": "6c250f83",
 		"/assets/logo_white.svg": "61d68fa0",
 		"/assets/vendor.42115f8b.js": "5d3951e5",
-		"/index.html": "ab59a3a2",
+		"/assets/vendor.ee7444dd.js": "5d3951e5",
+		"/index.html": "1a34b114",
 	}
 	resourcesMap = map[string]string{ 
 		"/assets/favicon.svg": "/assets/favicon.d0f5f504.svg",
@@ -25,7 +27,7 @@ var (
 		"/assets/logo_black.svg": "/assets/logo_black.svg",
 		"/assets/logo_orange.svg": "/assets/logo_orange.svg",
 		"/assets/logo_white.svg": "/assets/logo_white.svg",
-		"/assets/vendor.js": "/assets/vendor.42115f8b.js",
+		"/assets/vendor.js": "/assets/vendor.ee7444dd.js",
 		"/index.html": "/index.html",
 	}
 )
