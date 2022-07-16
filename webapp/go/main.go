7d1838a006459bfed7ea1dd6d02313036770d4b0
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-16 17:44:14 +0900

    perf: ⚡️ バリデーションは不要

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -894,9 +894,9 @@ func calculateGraphDataPoint(isuConditions []IsuCondition) (GraphDataPoint, erro
 	for _, condition := range isuConditions {
 		badConditionsCount := 0
 
-		if !isValidConditionFormat(condition.Condition) {
-			return GraphDataPoint{}, fmt.Errorf("invalid condition format")
-		}
+		//if !isValidConditionFormat(condition.Condition) {
+		//	return GraphDataPoint{}, fmt.Errorf("invalid condition format")
+		//}
 
 		for _, condStr := range strings.Split(condition.Condition, ",") {
 			keyValue := strings.Split(condStr, "=")
